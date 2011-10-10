07fc7900cf3819dbd8ab02155d1fef850643dca2
Larry Kuhns <lakuhns@embarqmail.com>
2011-10-10 08:37:22 -0400

    Aligned section names to left edge of stave
    Modified "stavesection.js" to align section names to the left edge
    of stave and fixed 'measureText" prototype in "raphaelcontext.js"
    to remove small printing at top left of Raphael context display.
    
    Updated "transform.htm;" and "vexflow_font" with glyphs for
    various strokes (brush, roll & rasquedo).

--- src/stavesection.js
+++ src/stavesection.js
@@ -57,7 +57,7 @@ Vex.Flow.StaveSection.prototype.draw = function(stave, shift_x) {
   var height = 20;
     //  Seems to be a good default y
   var y = stave.getYForTopText(3) + this.shift_y;
-  var x = this.x + shift_x - width / 2;
+  var x = this.x + shift_x;
   ctx.beginPath();
   ctx.lineWidth = 2;
   ctx.rect(x, y, width, height);
