3d33cba338c18d1a16025b8f1d279567a6cff749
Mohit Cheppudira <mohit@muthanna.com>
2012-04-23 10:31:09 -0400

    Wrote tool to find missing includes across codebase.

---
+++ tools/find_missing_includes.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby -w
+
+# VexFlow - Copyright Mohit Muthanna Cheppudira 2012
+#
+# This script verifies that the .js files in /src are included in all the
+# right places, e.g., SCons rules, HTML files, etc.
+
+require 'fileutils'
+require 'pp'
+
+include FileUtils
+
+SRCDIR = "../src"
+SCONSTRUCT = "#{SRCDIR}/SConstruct"
+
+HTML_FILES = [
+  "../tests/flow.html", "../docs/tutorial.html",
+  "../tabdiv/playground.html", "../tabdiv/tutorial.html",
+  "../tabdiv/vextab.html" ]
+
+unless File.exists?(SRCDIR)
+  puts "This tool needs to be run from the tools/ directory."
+  exit 1
+end
+
+
+# Extract .js files from SCONSTRUCT
+includes = File.readlines(SCONSTRUCT).
+                grep(/^\s+.*\.js.*/).
+                map { |f| f.chomp.gsub(/^.*"(.+)".*$/, '\1') }
+
+# Extract all .js files in src/
+files = Dir.glob("#{SRCDIR}/*.js").map { |f| f.gsub(SRCDIR + "/", "")}
+
+
+puts "Files in SCONSTRUCT but not in /src:"
+pp includes - files
+
+puts "\nFiles in /src but not in SCONSTRUCT"
+pp files - includes
+
+
+# Work through HTML files
+
+HTML_FILES.each do |html|
+  test_includes = File.readlines(html).
+                       grep(/^.*script\ssrc.*\.\.\/src.*\.js.*/).
+                       map { |f| f.chomp.gsub(/^.*"(.+)".*$/, '\1').gsub(SRCDIR + "/", "") }
+
+  puts "\nFiles in /src but not in #{html}:"
+  pp files - test_includes
+end
