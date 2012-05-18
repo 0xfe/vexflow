83ab1d3316239abaeb258054091c489b4543b2f8
Rob Buskens <rob@buskens.ca>
2012-05-17 19:35:19 -0500

    added minor pentatonic and major pentatonic.

--- src/music.js
+++ src/music.js
@@ -73,10 +73,18 @@ Vex.Flow.Music.intervals = {
 
 Vex.Flow.Music.scales = {
   major: [2, 2, 1, 2, 2, 2, 1],
+  ionian: [2, 2, 1, 2, 2, 2, 1],
   dorian: [2, 1, 2, 2, 2, 1, 2],
+  phrygian: [1, 2, 2, 2, 1, 2, 2],
+  lydian: [2, 2, 2, 1, 2, 2, 1],
   mixolydian: [2, 2, 1, 2, 2, 1, 2],
-  minor: [2, 1, 2, 2, 1, 2, 2]
+  minor: [2, 1, 2, 2, 1, 2, 2],
+  aeolian: [2, 1, 2, 2, 1, 2, 2],
+  locrian: [1, 2, 2, 1, 2, 2, 2],
+  majorPentatonic: [2, 2, 3, 2],
+  minorPentatonic: [3, 2, 2, 3]
 };
+
 
 Vex.Flow.Music.accidentals = [ "bb", "b", "n", "#", "##" ];
 
