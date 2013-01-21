9254c9fbd41aeab2aebedda1a4e4dbd993bd5174
Larry Kuhns <LarryKu@hotmail.com>
2013-01-21 14:41:59 -0500

    Implement Cautionary/Curtesy Accidentals
    accidentals.js - add setAsCautionary function and modify draw function
    to draw a smaller accidental surrounded by parentheses.
    
    tabls.js - add left and right paren to accidentalCodes.
    
    accidental_tests.js - add sample cautionary accidentals to basic test.

--- tests/accidental_tests.js
+++ tests/accidental_tests.js
@@ -72,6 +72,14 @@ Vex.Flow.Test.Accidental.basic = function(options, contextBuilder) {
       addAccidental(4, newAcc("bb")).
       addAccidental(5, newAcc("##")).
       addAccidental(6, newAcc("#")),
+
+    newNote({ keys: ["a/3", "c/4", "e/4", "b/4", "d/5", "g/5"], duration: "w"}).
+      addAccidental(0, newAcc("#")).
+      addAccidental(1, newAcc("##").setAsCautionary()).
+      addAccidental(2, newAcc("#").setAsCautionary()).
+      addAccidental(3, newAcc("b")).
+      addAccidental(4, newAcc("bb").setAsCautionary()).
+      addAccidental(5, newAcc("b").setAsCautionary()),
   ];
 
   for (var i = 0; i < notes.length; ++i) {
