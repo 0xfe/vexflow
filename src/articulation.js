3bda041b2a6115a197d932c09d7a01413fc0e7a1
Mohit Cheppudira <mohit@muthanna.com>
2013-10-15 10:15:03 -0400

    Refactored BarNote.

--- src/articulation.js
+++ src/articulation.js
@@ -6,128 +6,132 @@
 /**
  * @constructor
  */
-Vex.Flow.Articulation = function(type) {
-  if (arguments.length > 0) this.init(type);
-}
-Vex.Flow.Articulation.prototype = new Vex.Flow.Modifier();
-Vex.Flow.Articulation.prototype.constructor = Vex.Flow.Articulation;
-Vex.Flow.Articulation.superclass = Vex.Flow.Modifier.prototype;
-
-Vex.Flow.Articulation.prototype.init = function(type) {
-  var superclass = Vex.Flow.Articulation.superclass;
-  superclass.init.call(this);
-
-  this.note = null;
-  this.index = null;
-  this.type = type;
-  this.position = Vex.Flow.Modifier.Position.BELOW;
-
-  this.render_options = {
-    font_scale: 38,
-    stroke_px: 3,
-    stroke_spacing: 10
-  };
-
-  this.articulation = Vex.Flow.articulationCodes(this.type);
-  if (!this.articulation) throw new Vex.RERR("InvalidArticulation",
-     "Articulation not found: '" + this.type + "'");
-
-  this.setWidth(this.articulation.width);
-}
-
-Vex.Flow.Articulation.prototype.getCategory = function() {
-  return "articulations";
-}
-Vex.Flow.Articulation.prototype.getPosition = function() {
-  return this.position;
-}
-Vex.Flow.Articulation.prototype.setPosition = function(position) {
-  if (position == Vex.Flow.Modifier.Position.ABOVE ||
-      position == Vex.Flow.Modifier.Position.BELOW)
-    this.position = position;
-  return this;
-}
-
-Vex.Flow.Articulation.prototype.draw = function() {
-  if (!this.context) throw new Vex.RERR("NoContext",
-    "Can't draw Articulation without a context.");
-  if (!(this.note && (this.index !== null))) throw new Vex.RERR("NoAttachedNote",
-    "Can't draw Articulation without a note and index.");
-
-  var is_on_head = (this.position === Vex.Flow.Modifier.Position.ABOVE &&
-                    this.note.stem_direction === Vex.Flow.StaveNote.STEM_DOWN) ||
-                   (this.position === Vex.Flow.Modifier.Position.BELOW &&
-                    this.note.stem_direction === Vex.Flow.StaveNote.STEM_UP);
-  
-  var needsLineAdjustment = function(articulation, note_line, line_spacing){
-    var offset_direction = (articulation.position === Vex.Flow.Modifier.Position.ABOVE) ? 1 : -1;
-    
-    if(!is_on_head && articulation.note.duration !== "w"){
-      // Add stem length, inless it's on a whole note
-      note_line += offset_direction * 3.5;
-    }
-    var articulation_line = note_line + (offset_direction * line_spacing);
-    
-    if(articulation_line >= 1 &&
-       articulation_line <= 5 &&
-       articulation_line % 1 === 0){
-      return true;
-    }
-
-    return false;
+Vex.Flow.Articulation = (function() {
+  function Articulation(type) {
+    if (arguments.length > 0) this.init(type);
   }
 
-  // Articulations are centered over/under the note head
-  var stave = this.note.stave;
-  var start = this.note.getModifierStartXY(this.position, this.index);
-  var glyph_y = start.y;
-  var shiftY = 0;
-  var line_spacing = 1;
-  var spacing = stave.options.spacing_between_lines_px;
-  var top, bottom;
+  var Modifier = Vex.Flow.Modifier;
+  Vex.Inherit(Articulation, Modifier, {
+    init: function(type) {
+      Articulation.superclass.init.call(this);
 
-  if (this.note.getStemExtents) {
-    var stem_ext = this.note.getStemExtents();
-    top = stem_ext.topY;
-    bottom = stem_ext.baseY;
-    if (this.note.stem_direction === Vex.Flow.StaveNote.STEM_DOWN) {
-      top = stem_ext.baseY;
-      bottom = stem_ext.topY;
+      this.note = null;
+      this.index = null;
+      this.type = type;
+      this.position = Modifier.Position.BELOW;
+
+      this.render_options = {
+        font_scale: 38,
+        stroke_px: 3,
+        stroke_spacing: 10
+      };
+
+      this.articulation = Vex.Flow.articulationCodes(this.type);
+      if (!this.articulation) throw new Vex.RERR("InvalidArticulation",
+         "Articulation not found: '" + this.type + "'");
+
+      this.setWidth(this.articulation.width);
+    },
+
+    getCategory: function() {
+      return "articulations";
+    },
+
+    getPosition: function() {
+      return this.position;
+    },
+
+    setPosition: function(position) {
+      if (position == Vex.Flow.Modifier.Position.ABOVE ||
+          position == Vex.Flow.Modifier.Position.BELOW)
+        this.position = position;
+      return this;
+    },
+
+    draw: function() {
+      if (!this.context) throw new Vex.RERR("NoContext",
+        "Can't draw Articulation without a context.");
+      if (!(this.note && (this.index !== null))) throw new Vex.RERR("NoAttachedNote",
+        "Can't draw Articulation without a note and index.");
+
+      var is_on_head = (this.position === Modifier.Position.ABOVE &&
+                        this.note.stem_direction === Vex.Flow.StaveNote.STEM_DOWN) ||
+                       (this.position === Modifier.Position.BELOW &&
+                        this.note.stem_direction === Vex.Flow.StaveNote.STEM_UP);
+
+      var needsLineAdjustment = function(articulation, note_line, line_spacing){
+        var offset_direction = (articulation.position === Modifier.Position.ABOVE) ? 1 : -1;
+
+        if(!is_on_head && articulation.note.duration !== "w"){
+          // Add stem length, inless it's on a whole note
+          note_line += offset_direction * 3.5;
+        }
+        var articulation_line = note_line + (offset_direction * line_spacing);
+
+        if(articulation_line >= 1 &&
+           articulation_line <= 5 &&
+           articulation_line % 1 === 0){
+          return true;
+        }
+
+        return false;
+      }
+
+      // Articulations are centered over/under the note head
+      var stave = this.note.stave;
+      var start = this.note.getModifierStartXY(this.position, this.index);
+      var glyph_y = start.y;
+      var shiftY = 0;
+      var line_spacing = 1;
+      var spacing = stave.options.spacing_between_lines_px;
+      var top, bottom;
+
+      if (this.note.getStemExtents) {
+        var stem_ext = this.note.getStemExtents();
+        top = stem_ext.topY;
+        bottom = stem_ext.baseY;
+        if (this.note.stem_direction === Vex.Flow.StaveNote.STEM_DOWN) {
+          top = stem_ext.baseY;
+          bottom = stem_ext.topY;
+        }
+      }
+
+      var is_above = (this.position === Modifier.Position.ABOVE) ? true : false;
+      var note_line = this.note.getLineNumber(is_above);
+
+      // Beamed stems are longer than quarter note stems
+      if(!is_on_head && this.note.beam)
+        line_spacing += 0.5;
+
+      // If articulation will overlap a line, reposition it
+      if(needsLineAdjustment(this, note_line, line_spacing))
+        line_spacing += 0.5;
+
+      if (this.position === Modifier.Position.ABOVE) {
+        shiftY = this.articulation.shift_up;
+
+        var glyph_y_between_lines = (top - 7) - (spacing * (this.text_line + line_spacing));
+        if(this.articulation.between_lines)
+          glyph_y = glyph_y_between_lines;
+        else
+          glyph_y = Vex.Min(stave.getYForTopText(this.text_line) - 3, glyph_y_between_lines);
+      } else {
+        shiftY = this.articulation.shift_down - 10;
+
+        var glyph_y_between_lines = bottom + 10 + spacing * (this.text_line + line_spacing);
+        if(this.articulation.between_lines)
+         glyph_y = glyph_y_between_lines;
+        else
+          glyph_y = Vex.Max(stave.getYForBottomText(this.text_line), glyph_y_between_lines);
+      }
+
+      var glyph_x = start.x + this.articulation.shift_right;
+      glyph_y += shiftY + this.y_shift;
+
+     Vex.Flow.renderGlyph(this.context, glyph_x, glyph_y,
+                           this.render_options.font_scale, this.articulation.code);
     }
-  }
-
-  var is_above = (this.position === Vex.Flow.Modifier.Position.ABOVE) ? true : false;
-  var note_line = this.note.getLineNumber(is_above);
-
-  // Beamed stems are longer than quarter note stems
-  if(!is_on_head && this.note.beam)
-    line_spacing += 0.5;
-
-  // If articulation will overlap a line, reposition it
-  if(needsLineAdjustment(this, note_line, line_spacing))
-    line_spacing += 0.5;
-
-  if (this.position === Vex.Flow.Modifier.Position.ABOVE) {
-    shiftY = this.articulation.shift_up;
-
-    var glyph_y_between_lines = (top - 7) - (spacing * (this.text_line + line_spacing));
-    if(this.articulation.between_lines)
-      glyph_y = glyph_y_between_lines;
-    else
-      glyph_y = Vex.Min(stave.getYForTopText(this.text_line) - 3, glyph_y_between_lines);
-  } else {
-    shiftY = this.articulation.shift_down - 10;
-
-    var glyph_y_between_lines = bottom + 10 + spacing * (this.text_line + line_spacing);
-    if(this.articulation.between_lines)
-     glyph_y = glyph_y_between_lines;
-    else
-      glyph_y = Vex.Max(stave.getYForBottomText(this.text_line), glyph_y_between_lines);
-  }
-
-  var glyph_x = start.x + this.articulation.shift_right;
-  glyph_y += shiftY + this.y_shift;
-
- Vex.Flow.renderGlyph(this.context, glyph_x, glyph_y,
-                       this.render_options.font_scale, this.articulation.code);
-}
+  });
+  return Articulation;
+}());
--- src/barnote.js
+++ src/barnote.js
@@ -6,74 +6,66 @@
 // Requires vex.js.
 
 /** @constructor */
-Vex.Flow.BarNote = function() { this.init(); }
-Vex.Flow.BarNote.prototype = new Vex.Flow.Note();
-Vex.Flow.BarNote.superclass = Vex.Flow.Note.prototype;
-Vex.Flow.BarNote.constructor = Vex.Flow.BarNote;
+Vex.Flow.BarNote = (function() {
+  function BarNote() { this.init(); }
 
-Vex.Flow.BarNote.prototype.init = function() {
-  var superclass = Vex.Flow.BarNote.superclass;
-  superclass.init.call(this, {duration: "b"});
+  Vex.Inherit(BarNote, Vex.Flow.Note, {
+    init: function() {
+      BarNote.superclass.init.call(this, {duration: "b"});
 
-  var TYPE = Vex.Flow.Barline.type;
-  this.metrics = {
-    widths: {}
-  }
+      var TYPE = Vex.Flow.Barline.type;
+      this.metrics = {
+        widths: {}
+      }
 
-  this.metrics.widths[TYPE.SINGLE] = 8;
-  this.metrics.widths[TYPE.DOUBLE] = 12;
-  this.metrics.widths[TYPE.END] = 15;
-  this.metrics.widths[TYPE.REPEAT_BEGIN] = 14;
-  this.metrics.widths[TYPE.REPEAT_END] = 14;
-  this.metrics.widths[TYPE.REPEAT_BOTH] = 18;
-  this.metrics.widths[TYPE.NONE] = 0;
+      this.metrics.widths[TYPE.SINGLE] = 8;
+      this.metrics.widths[TYPE.DOUBLE] = 12;
+      this.metrics.widths[TYPE.END] = 15;
+      this.metrics.widths[TYPE.REPEAT_BEGIN] = 14;
+      this.metrics.widths[TYPE.REPEAT_END] = 14;
+      this.metrics.widths[TYPE.REPEAT_BOTH] = 18;
+      this.metrics.widths[TYPE.NONE] = 0;
 
-  // Note properties
-  this.ignore_ticks = true;
-  this.type = TYPE.SINGLE;
-  this.setWidth(this.metrics.widths[this.type]);
-}
+      // Note properties
+      this.ignore_ticks = true;
+      this.type = TYPE.SINGLE;
+      this.setWidth(this.metrics.widths[this.type]);
+    },
 
-Vex.Flow.BarNote.prototype.setType = function(type) {
-  this.type = type;
-  this.setWidth(this.metrics.widths[this.type]);
-  return this;
-}
+    setType: function(type) {
+      this.type = type;
+      this.setWidth(this.metrics.widths[this.type]);
+      return this;
+    },
 
-Vex.Flow.BarNote.prototype.getType = function() {
-  return this.type;
-}
+    getType: function() {
+      return this.type;
+    },
 
-Vex.Flow.BarNote.prototype.setStave = function(stave) {
-  var superclass = Vex.Flow.BarNote.superclass;
-  superclass.setStave.call(this, stave);
-}
+    setStave: function(stave) {
+      var superclass = Vex.Flow.BarNote.superclass;
+      superclass.setStave.call(this, stave);
+    },
 
-Vex.Flow.BarNote.prototype.getBoundingBox = function() {
-  return new Vex.Flow.BoundingBox(0, 0, 0, 0);
-}
+    getBoundingBox: function() {
+      return new Vex.Flow.BoundingBox(0, 0, 0, 0);
+    },
 
-Vex.Flow.BarNote.prototype.addToModifierContext = function(mc) {
-  return this;
-}
+    addToModifierContext: function(mc) {
+      return this;
+    },
 
-Vex.Flow.BarNote.prototype.preFormat = function() {
-  this.setPreFormatted(true);
-  return this;
-}
+    preFormat: function() {
+      this.setPreFormatted(true);
+      return this;
+    },
 
-Vex.Flow.BarNote.prototype.draw = function() {
-  if (!this.stave) throw new Vex.RERR("NoStave", "Can't draw without a stave.");
+    draw: function() {
+      if (!this.stave) throw new Vex.RERR("NoStave", "Can't draw without a stave.");
+      var barline = new Vex.Flow.Barline(this.type, this.getAbsoluteX());
+      barline.draw(this.stave, this.getAbsoluteX());
+    }
+  });
 
-  /*
-  var x = this.getAbsoluteX() + this.x_shift;
-  if (this.type == Vex.Flow.BarNote.TYPE.SINGLE) {
-    this.stave.drawVerticalBarFixed(x);
-  } else if (this.type == Vex.Flow.BarNote.TYPE.DOUBLE) {
-    this.stave.drawVerticalBarFixed(x);
-    this.stave.drawVerticalBarFixed(x + this.metrics.double_x_shift);
-  }
-  */
-  var barline = new Vex.Flow.Barline(this.type, this.getAbsoluteX());
-  barline.draw(this.stave, this.getAbsoluteX());
-}
+  return BarNote;
+}());
