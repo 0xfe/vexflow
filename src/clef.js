38645dce42bc725c694f5f2c7c07b4dc9fa9501a
Mohit Cheppudira <mohit@muthanna.com>
2013-10-15 10:34:32 -0400

    Refactored clefs.

--- src/clef.js
+++ src/clef.js
@@ -1,56 +1,54 @@
-// Vex Flow Notation
+// Vex Flow Notation.
+// Copyright Mohit Muthanna Cheppudira 2013.
 // Implements clefs
-//
-// Requires vex.js.
 
-/** 
- * @constructor 
- */
-Vex.Flow.Clef = function(clef) { 
-  if (arguments.length > 0) this.init(clef); 
-}
-
-Vex.Flow.Clef.types = {
-  "treble": {
-    code: "v83",
-    point: 40,
-    line: 3
-  },
-  "bass": {
-    code: "v79",
-    point: 40,
-    line: 1
-  },
-  "alto": {
-    code: "vad",
-    point: 40,
-    line: 2
-  },
-  "tenor": {
-    code: "vad",
-    point: 40,
-    line: 1
-  },
-  "percussion": {
-    code: "v59",
-    point: 40,
-    line: 2
+Vex.Flow.Clef = (function() {
+  function Clef(clef) {
+    if (arguments.length > 0) this.init(clef); 
   }
-};
 
-Vex.Flow.Clef.prototype = new Vex.Flow.StaveModifier();
-Vex.Flow.Clef.prototype.constructor = Vex.Flow.Clef;
-Vex.Flow.Clef.superclass = Vex.Flow.StaveModifier.prototype;
+  Clef.types = {
+    "treble": {
+      code: "v83",
+      point: 40,
+      line: 3
+    },
+    "bass": {
+      code: "v79",
+      point: 40,
+      line: 1
+    },
+    "alto": {
+      code: "vad",
+      point: 40,
+      line: 2
+    },
+    "tenor": {
+      code: "vad",
+      point: 40,
+      line: 1
+    },
+    "percussion": {
+      code: "v59",
+      point: 40,
+      line: 2
+    }
+  };
 
-Vex.Flow.Clef.prototype.init = function(clef) {
-  var superclass = Vex.Flow.Clef.superclass;
-  superclass.init.call(this);
+  Vex.Inherit(Clef, Vex.Flow.StaveModifier, {
+    init: function(clef) {
+      var superclass = Vex.Flow.Clef.superclass;
+      superclass.init.call(this);
 
-  this.clef = Vex.Flow.Clef.types[clef];
-}
+      this.clef = Vex.Flow.Clef.types[clef];
+    },
 
-Vex.Flow.Clef.prototype.addModifier = function(stave) {
-  var glyph = new Vex.Flow.Glyph(this.clef.code, this.clef.point);
-  this.placeGlyphOnLine(glyph, stave, this.clef.line);
-  stave.addGlyph(glyph);
-}
+    addModifier: function(stave) {
+      var glyph = new Vex.Flow.Glyph(this.clef.code, this.clef.point);
+      this.placeGlyphOnLine(glyph, stave, this.clef.line);
+      stave.addGlyph(glyph);
+    }
+  });
+
+  return Clef;
+}());
