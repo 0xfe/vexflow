54c75f3c646eb6566d389846669815d88ffdb6db
Benjamin W. Bohl <bohl@edirom.de>
2014-04-03 17:27:57 +0200

    added support for clefs
    * soprano
    * mezzo-soprano
    * baritone-c
    * baritone-f
    * subbass
    * french
    
    also updated celf_test.js
    
    in particular refacored clef change test to show c4 for every clef
    change, easing evaluation of correct note placement

--- tests/clef_tests.js
+++ tests/clef_tests.js
@@ -15,18 +15,30 @@ Vex.Flow.Test.Clef.Start = function() {
 }
 
 Vex.Flow.Test.Clef.draw = function(options, contextBuilder) {
-  var ctx = new contextBuilder(options.canvas_sel, 400, 120);
-  var stave = new Vex.Flow.Stave(10, 10, 300);
+  var ctx = new contextBuilder(options.canvas_sel, 800, 120);
+  var stave = new Vex.Flow.Stave(10, 10, 700);
 
   stave.addClef("treble");
   stave.addClef("alto");
   stave.addClef("tenor");
+  stave.addClef("soprano");
   stave.addClef("bass");
+  stave.addClef("mezzo-soprano");
+  stave.addClef("baritone-c");
+  stave.addClef("baritone-f");
+  stave.addClef("subbass");
+  stave.addClef("french");
 
   stave.addEndClef("treble");
   stave.addEndClef("alto");
   stave.addEndClef("tenor");
+  stave.addEndClef("soprano");
   stave.addEndClef("bass");
+  stave.addEndClef("mezzo-soprano");
+  stave.addEndClef("baritone-c");
+  stave.addEndClef("baritone-f");
+  stave.addEndClef("subbass");
+  stave.addEndClef("french");
 
   stave.setContext(ctx);
   stave.draw();
@@ -35,18 +47,30 @@ Vex.Flow.Test.Clef.draw = function(options, contextBuilder) {
 }
 
 Vex.Flow.Test.Clef.drawSmall = function(options, contextBuilder) {
-  var ctx = new contextBuilder(options.canvas_sel, 400, 120);
-  var stave = new Vex.Flow.Stave(10, 10, 300);
+  var ctx = new contextBuilder(options.canvas_sel, 800, 120);
+  var stave = new Vex.Flow.Stave(10, 10, 700);
 
   stave.addClef("treble_small");
   stave.addClef("alto_small");
   stave.addClef("tenor_small");
+  stave.addClef("soprano_small");
   stave.addClef("bass_small");
+  stave.addClef("mezzo-soprano_small");
+  stave.addClef("baritone-c_small");
+  stave.addClef("baritone-f_small");
+  stave.addClef("subbass_small");
+  stave.addClef("french_small");
 
   stave.addEndClef("treble_small");
   stave.addEndClef("alto_small");
   stave.addEndClef("tenor_small");
+  stave.addEndClef("soprano_small");
   stave.addEndClef("bass_small");
+  stave.addEndClef("mezzo-soprano_small");
+  stave.addEndClef("baritone-c_small");
+  stave.addEndClef("baritone-f_small");
+  stave.addEndClef("subbass_small");
+  stave.addEndClef("french_small");
 
   stave.setContext(ctx);
   stave.draw();
@@ -55,21 +79,34 @@ Vex.Flow.Test.Clef.drawSmall = function(options, contextBuilder) {
 }
 
 Vex.Flow.Test.Clef.drawClefChange = function(options, contextBuilder) {
-  var ctx = new contextBuilder(options.canvas_sel, 600, 120);
-  var stave = new Vex.Flow.Stave(10, 10, 500);
+  var ctx = new contextBuilder(options.canvas_sel, 800, 120);
+  var stave = new Vex.Flow.Stave(10, 10, 700);
   stave.addClef("treble").setContext(ctx).draw();
 
   var notes = [
     new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q", clef: "treble" }),
     new Vex.Flow.ClefNote("alto_small"),
-    new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "q", clef: "alto" }),
-    new Vex.Flow.StaveNote({ keys: ["b/3"], duration: "qr", clef: "alto" }),
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q", clef: "alto" }),
+    new Vex.Flow.ClefNote("tenor_small"),
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q", clef: "tenor" }),
+    new Vex.Flow.ClefNote("soprano_small"),
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q", clef: "soprano" }),
     new Vex.Flow.ClefNote("bass_small"),
-    new Vex.Flow.StaveNote({ keys: ["c/3", "e/3", "g/3"], duration: "q", clef: "bass" }),
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q", clef: "bass" }),
+    new Vex.Flow.ClefNote("mezzo-soprano_small"),
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q", clef: "mezzo-soprano" }),
+    new Vex.Flow.ClefNote("baritone-c_small"),
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q", clef: "baritone-c" }),
+    new Vex.Flow.ClefNote("baritone-f_small"),
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q", clef: "baritone-f" }),
+    new Vex.Flow.ClefNote("subbass_small"),
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q", clef: "subbass" }),
+    new Vex.Flow.ClefNote("french_small"),
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q", clef: "french" })
   ];
 
   var voice = new Vex.Flow.Voice({
-    num_beats: 4,
+    num_beats: 10,
     beat_value: 4,
     resolution: Vex.Flow.RESOLUTION
   });
