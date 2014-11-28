080195a71cc49d9443f6d802e4553266cfa89b19
Mohit Cheppudira <mohit@muthanna.com>
2014-11-28 08:14:22 -0500

    Committing release binaries for new version: 1.2.25

--- releases/vexflow-debug.js
+++ releases/vexflow-debug.js
@@ -1,5 +1,5 @@
 /**
- * VexFlow 1.2.24 built on 2014-11-28.
+ * VexFlow 1.2.25 built on 2014-11-28.
  * Copyright (c) 2010 Mohit Muthanna Cheppudira <mohit@muthanna.com>
  *
  * http://www.vexflow.com  http://github.com/0xfe/vexflow
