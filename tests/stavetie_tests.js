3d47320d3b29714b8c6f091fdb6641faa1587ac1
Mohit Cheppudira <mohit@muthanna.com>
2015-10-13 15:15:42 -0400

    Refactored stavenote, modifiers, stavetie, and stringnumber.

--- tests/stavetie_tests.js
+++ tests/stavetie_tests.js
@@ -3,150 +3,158 @@
  * Copyright Mohit Muthanna 2010 <mohit@muthanna.com>
  */
 
-Vex.Flow.Test.StaveTie = {}
+VF.Test.StaveTie = (function() {
+  var StaveTie = {
+    Start: function() {
+      var runTests = VF.Test.runTests;
 
-Vex.Flow.Test.StaveTie.Start = function() {
-  module("StaveTie");
-  Vex.Flow.Test.runTests("Simple StaveTie", Vex.Flow.Test.StaveTie.simple);
-  Vex.Flow.Test.runTests("Chord StaveTie", Vex.Flow.Test.StaveTie.chord);
-  Vex.Flow.Test.runTests("Stem Up StaveTie", Vex.Flow.Test.StaveTie.stemUp);
-  Vex.Flow.Test.runTests("No End Note", Vex.Flow.Test.StaveTie.noEndNote);
-  Vex.Flow.Test.runTests("No Start Note", Vex.Flow.Test.StaveTie.noStartNote);
-}
+      QUnit.module("StaveTie");
+      runTests("Simple StaveTie", StaveTie.simple);
+      runTests("Chord StaveTie", StaveTie.chord);
+      runTests("Stem Up StaveTie", StaveTie.stemUp);
+      runTests("No End Note", StaveTie.noEndNote);
+      runTests("No Start Note", StaveTie.noStartNote);
+    },
 
-Vex.Flow.Test.StaveTie.tieNotes = function(notes, indices, stave, ctx) {
-  var voice = new Vex.Flow.Voice(Vex.Flow.Test.TIME4_4);
-  voice.addTickables(notes);
+    tieNotes: function(notes, indices, stave, ctx) {
+      var voice = new VF.Voice(VF.Test.TIME4_4);
+      voice.addTickables(notes);
 
-  var formatter = new Vex.Flow.Formatter().joinVoices([voice]).
-    format([voice], 250);
-  voice.draw(ctx, stave);
+      var formatter = new VF.Formatter().joinVoices([voice]).
+        format([voice], 250);
+      voice.draw(ctx, stave);
 
-  var tie = new Vex.Flow.StaveTie({
-    first_note: notes[0],
-    last_note: notes[1],
-    first_indices: indices,
-    last_indices: indices,
-  });
+      var tie = new VF.StaveTie({
+        first_note: notes[0],
+        last_note: notes[1],
+        first_indices: indices,
+        last_indices: indices,
+      });
 
-  tie.setContext(ctx);
-  tie.draw();
-}
+      tie.setContext(ctx);
+      tie.draw();
+    },
 
-Vex.Flow.Test.StaveTie.drawTie = function(notes, indices, options) {
-  var ctx = new options.contextBuilder(options.canvas_sel, 350, 140);
+    drawTie: function(notes, indices, options) {
+      var ctx = new options.contextBuilder(options.canvas_sel, 350, 140);
 
-  ctx.scale(0.9, 0.9); ctx.fillStyle = "#221"; ctx.strokeStyle = "#221";
-  var stave = new Vex.Flow.Stave(10, 10, 350).setContext(ctx).draw();
+      ctx.scale(0.9, 0.9); ctx.fillStyle = "#221"; ctx.strokeStyle = "#221";
+      var stave = new VF.Stave(10, 10, 350).setContext(ctx).draw();
 
-  Vex.Flow.Test.StaveTie.tieNotes(notes, indices, stave, ctx);
-}
+      VF.Test.StaveTie.tieNotes(notes, indices, stave, ctx);
+    },
 
-Vex.Flow.Test.StaveTie.simple = function(options, contextBuilder) {
-  options.contextBuilder = contextBuilder;
-  function newNote(note_struct) { return new Vex.Flow.StaveNote(note_struct); }
-  function newAcc(type) { return new Vex.Flow.Accidental(type); }
+    simple: function(options, contextBuilder) {
+      options.contextBuilder = contextBuilder;
+      function newNote(note_struct) { return new VF.StaveNote(note_struct); }
+      function newAcc(type) { return new VF.Accidental(type); }
 
-  Vex.Flow.Test.StaveTie.drawTie([
-    newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "h"}).
-      addAccidental(0, newAcc("b")).
-      addAccidental(1, newAcc("#")),
-    newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"})
-  ], [0, 1], options);
-  ok(true, "Simple Test");
-}
+      VF.Test.StaveTie.drawTie([
+        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "h"}).
+          addAccidental(0, newAcc("b")).
+          addAccidental(1, newAcc("#")),
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"})
+      ], [0, 1], options);
+      ok(true, "Simple Test");
+    },
 
-Vex.Flow.Test.StaveTie.chord = function(options, contextBuilder) {
-  options.contextBuilder = contextBuilder;
-  function newNote(note_struct) { return new Vex.Flow.StaveNote(note_struct); }
-  function newAcc(type) { return new Vex.Flow.Accidental(type); }
+    chord: function(options, contextBuilder) {
+      options.contextBuilder = contextBuilder;
+      function newNote(note_struct) { return new VF.StaveNote(note_struct); }
+      function newAcc(type) { return new VF.Accidental(type); }
 
-  Vex.Flow.Test.StaveTie.drawTie([
-    newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"}),
-    newNote({ keys: ["c/4", "f/4", "a/4"], stem_direction: -1, duration: "h"}).
-      addAccidental(0, newAcc("n")).
-      addAccidental(1, newAcc("#")),
-  ], [0, 1, 2], options);
-  ok(true, "Chord test");
-}
+      VF.Test.StaveTie.drawTie([
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"}),
+        newNote({ keys: ["c/4", "f/4", "a/4"], stem_direction: -1, duration: "h"}).
+          addAccidental(0, newAcc("n")).
+          addAccidental(1, newAcc("#")),
+      ], [0, 1, 2], options);
+      ok(true, "Chord test");
+    },
 
-Vex.Flow.Test.StaveTie.stemUp = function(options, contextBuilder) {
-  options.contextBuilder = contextBuilder;
-  function newNote(note_struct) { return new Vex.Flow.StaveNote(note_struct); }
-  function newAcc(type) { return new Vex.Flow.Accidental(type); }
+    stemUp: function(options, contextBuilder) {
+      options.contextBuilder = contextBuilder;
+      function newNote(note_struct) { return new VF.StaveNote(note_struct); }
+      function newAcc(type) { return new VF.Accidental(type); }
 
-  Vex.Flow.Test.StaveTie.drawTie([
-    newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: 1, duration: "h"}),
-    newNote({ keys: ["c/4", "f/4", "a/4"], stem_direction: 1, duration: "h"}).
-      addAccidental(0, newAcc("n")).
-      addAccidental(1, newAcc("#")),
-  ], [0, 1, 2], options);
-  ok(true, "Stem up test");
-}
+      VF.Test.StaveTie.drawTie([
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: 1, duration: "h"}),
+        newNote({ keys: ["c/4", "f/4", "a/4"], stem_direction: 1, duration: "h"}).
+          addAccidental(0, newAcc("n")).
+          addAccidental(1, newAcc("#")),
+      ], [0, 1, 2], options);
+      ok(true, "Stem up test");
+    },
 
-Vex.Flow.Test.StaveTie.noEndNote = function(options, contextBuilder) {
-  var ctx = contextBuilder(options.canvas_sel, 350, 140);
-  ctx.scale(0.9, 0.9); ctx.fillStyle = "#221"; ctx.strokeStyle = "#221";
-  ctx.font = "10pt Arial";
-  var stave = new Vex.Flow.Stave(10, 10, 350).setContext(ctx).draw();
+    noEndNote: function(options, contextBuilder) {
+      var ctx = contextBuilder(options.canvas_sel, 350, 140);
+      ctx.scale(0.9, 0.9); ctx.fillStyle = "#221"; ctx.strokeStyle = "#221";
+      ctx.font = "10pt Arial";
+      var stave = new VF.Stave(10, 10, 350).setContext(ctx).draw();
 
-  function newNote(note_struct) { return new Vex.Flow.StaveNote(note_struct); }
-  function newAcc(type) { return new Vex.Flow.Accidental(type); }
+      function newNote(note_struct) { return new VF.StaveNote(note_struct); }
+      function newAcc(type) { return new VF.Accidental(type); }
 
-  var notes = [
-    newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "h"}).
-      addAccidental(0, newAcc("b")).
-      addAccidental(1, newAcc("#")),
-    newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"})
-  ];
+      var notes = [
+        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "h"}).
+          addAccidental(0, newAcc("b")).
+          addAccidental(1, newAcc("#")),
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"})
+      ];
 
-  var voice = new Vex.Flow.Voice(Vex.Flow.Test.TIME4_4);
-  voice.addTickables(notes);
+      var voice = new VF.Voice(VF.Test.TIME4_4);
+      voice.addTickables(notes);
 
-  var formatter = new Vex.Flow.Formatter().joinVoices([voice]).
-    format([voice], 250);
-  voice.draw(ctx, stave);
+      var formatter = new VF.Formatter().joinVoices([voice]).
+        format([voice], 250);
+      voice.draw(ctx, stave);
 
-  var tie = new Vex.Flow.StaveTie({
-    first_note: notes[1],
-    last_note: null,
-    first_indices: [2],
-    last_indices: [2]
-  }, "slow.").setContext(ctx).draw();
+      var tie = new VF.StaveTie({
+        first_note: notes[1],
+        last_note: null,
+        first_indices: [2],
+        last_indices: [2]
+      }, "slow.").setContext(ctx).draw();
 
-  ok(true, "No end note");
-}
+      ok(true, "No end note");
+    },
 
-Vex.Flow.Test.StaveTie.noStartNote = function(options, contextBuilder) {
-  var ctx = contextBuilder(options.canvas_sel, 350, 140);
-  ctx.scale(0.9, 0.9); ctx.fillStyle = "#221"; ctx.strokeStyle = "#221";
-  ctx.font = "10pt Arial";
-  var stave = new Vex.Flow.Stave(10, 10, 350).addTrebleGlyph().
-    setContext(ctx).draw();
+    noStartNote: function(options, contextBuilder) {
+      var ctx = contextBuilder(options.canvas_sel, 350, 140);
+      ctx.scale(0.9, 0.9); ctx.fillStyle = "#221"; ctx.strokeStyle = "#221";
+      ctx.font = "10pt Arial";
+      var stave = new VF.Stave(10, 10, 350).addTrebleGlyph().
+        setContext(ctx).draw();
 
-  function newNote(note_struct) { return new Vex.Flow.StaveNote(note_struct); }
-  function newAcc(type) { return new Vex.Flow.Accidental(type); }
+      function newNote(note_struct) { return new VF.StaveNote(note_struct); }
+      function newAcc(type) { return new VF.Accidental(type); }
 
-  var notes = [
-    newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "h"}).
-      addAccidental(0, newAcc("b")).
-      addAccidental(1, newAcc("#")),
-    newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"})
-  ];
+      var notes = [
+        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "h"}).
+          addAccidental(0, newAcc("b")).
+          addAccidental(1, newAcc("#")),
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"})
+      ];
 
-  var voice = new Vex.Flow.Voice(Vex.Flow.Test.TIME4_4);
-  voice.addTickables(notes);
+      var voice = new VF.Voice(VF.Test.TIME4_4);
+      voice.addTickables(notes);
 
-  var formatter = new Vex.Flow.Formatter().joinVoices([voice]).
-    format([voice], 250);
-  voice.draw(ctx, stave);
+      var formatter = new VF.Formatter().joinVoices([voice]).
+        format([voice], 250);
+      voice.draw(ctx, stave);
 
-  var tie = new Vex.Flow.StaveTie({
-    first_note: null,
-    last_note: notes[0],
-    first_indices: [2],
-    last_indices: [2],
-  }, "H").setContext(ctx).draw();
+      var tie = new VF.StaveTie({
+        first_note: null,
+        last_note: notes[0],
+        first_indices: [2],
+        last_indices: [2],
+      }, "H").setContext(ctx).draw();
 
-  ok(true, "No end note");
-}
+      ok(true, "No end note");
+    }
+  };
+
+  return StaveTie;
+})();
+
+module.exports = VF.Test.StaveTie;
