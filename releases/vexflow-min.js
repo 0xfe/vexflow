2259f3165cc06157f286794ba688840fcfc6a0ad
infojunkie <karim.ratib@gmail.com>
2016-03-12 08:50:46 -0800

    Release v1.2.42

--- releases/vexflow-min.js
+++ releases/vexflow-min.js
@@ -1,5 +1,5 @@
 /**
- * VexFlow 1.2.41 built on 2016-03-10.
+ * VexFlow 1.2.42 built on 2016-03-12.
  * Copyright (c) 2010 Mohit Muthanna Cheppudira <mohit@muthanna.com>
  *
  * http://www.vexflow.com  http://github.com/0xfe/vexflow
