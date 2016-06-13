de9fe09625d3cdb1c6c6268b393e52ef237885e8
Cyril Silverman <cyril.silverman@tido-music.com>
2016-06-13 01:24:01 +0100

    Clean up

--- src/boundingboxcomputation.js
+++ src/boundingboxcomputation.js
@@ -58,34 +58,33 @@ Vex.Flow.BoundingBoxComputation = (function() {
 
     addBezierCurve: function (p0x, p0y, p1x, p1y, p2x, p2y, p3x, p3y) {
       // from http://blog.hackers-cafe.net/2009/06/how-to-calculate-bezier-curves-bounding.html
-      var
-        i,
-        p0 = [p0x, p0y],
-        p1 = [p1x, p1y],
-        p2 = [p2x, p2y],
-        p3 = [p3x, p3y];
+      var p0 = [p0x, p0y];
+      var p1 = [p1x, p1y];
+      var p2 = [p2x, p2y];
+      var p3 = [p3x, p3y];
+      var i;
 
       this.addPoint(p0[0], p0[1]);
       this.addPoint(p3[0], p3[1]);
 
-      for (i = 0; i <= 1; i++) {
-        var f = function (t) {
-          return Math.pow(1 - t, 3) * p0[i]
-            + 3 * Math.pow(1 - t, 2) * t * p1[i]
-            + 3 * (1 - t) * Math.pow(t, 2) * p2[i]
-            + Math.pow(t, 3) * p3[i];
-        };
+      var f = function (t, i) {
+        return Math.pow(1 - t, 3) * p0[i] +
+          3 * Math.pow(1 - t, 2) * t * p1[i] +
+          3 * (1 - t) * Math.pow(t, 2) * p2[i] +
+          Math.pow(t, 3) * p3[i];
+      };
 
+      for (i = 0; i <= 1; i++) {
         var b = 6 * p0[i] - 12 * p1[i] + 6 * p2[i];
         var a = -3 * p0[i] + 9 * p1[i] - 9 * p2[i] + 3 * p3[i];
         var c = 3 * p1[i] - 3 * p0[i];
 
-        if (a == 0) {
-          if (b == 0) continue;
+        if (a === 0) {
+          if (b === 0) continue;
           var t = -c / b;
           if (0 < t && t < 1) {
-            if (i == 0) this.addX(f(t));
-            if (i == 1) this.addY(f(t));
+            if (i === 0) this.addX(f(t, i));
+            if (i === 1) this.addY(f(t, i));
           }
           continue;
         }
@@ -94,13 +93,13 @@ Vex.Flow.BoundingBoxComputation = (function() {
         if (b2ac < 0) continue;
         var t1 = (-b + Math.sqrt(b2ac)) / (2 * a);
         if (0 < t1 && t1 < 1) {
-          if (i == 0) this.addX(f(t1));
-          if (i == 1) this.addY(f(t1));
+          if (i === 0) this.addX(f(t1, i));
+          if (i === 1) this.addY(f(t1, i));
         }
         var t2 = (-b - Math.sqrt(b2ac)) / (2 * a);
         if (0 < t2 && t2 < 1) {
-          if (i == 0) this.addX(f(t2));
-          if (i == 1) this.addY(f(t2));
+          if (i === 0) this.addX(f(t2, i));
+          if (i === 1) this.addY(f(t2, i));
         }
       }
     }
