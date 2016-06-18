ab6b9fdc297627d5481b9b294cd549a69789f0f2
Mohit Cheppudira <mohit@muthanna.com>
2016-06-18 09:24:58 -0400

    Committing release binaries for new version: 1.2.49

--- releases/vexflow-tests.js
+++ releases/vexflow-tests.js
@@ -1,5 +1,5 @@
 /**
- * VexFlow 1.2.48 built on 2016-06-17.
+ * VexFlow 1.2.49 built on 2016-06-18.
  * Copyright (c) 2010 Mohit Muthanna Cheppudira <mohit@muthanna.com>
  *
  * http://www.vexflow.com  http://github.com/0xfe/vexflow
