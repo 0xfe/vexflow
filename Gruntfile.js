eeae0f0d831c73105fa2b4dceec300814dd9cb4b
Cyril Silverman <cyril.silverman@tido-music.com>
2016-06-23 21:49:23 +0100

    Lint fixes and slight refactoring in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,15 @@
+const babel = require('rollup-plugin-babel');
+const path = require('path');
+
 module.exports = (grunt) => {
-  const path = require('path');
-  const L = grunt.log.writeln;
-  const BANNER = '/**\n' +
-                ' * VexFlow <%= pkg.version %> built on <%= grunt.template.today("yyyy-mm-dd") %>.\n' +
-                ' * Copyright (c) 2010 Mohit Muthanna Cheppudira <mohit@muthanna.com>\n' +
-                ' *\n' +
-                ' * http://www.vexflow.com  http://github.com/0xfe/vexflow\n' +
-                ' */\n';
+  const BANNER = [
+    '/**',
+    ' * VexFlow <%= pkg.version %> built on <%= grunt.template.today("yyyy-mm-dd") %>.',
+    ' * Copyright (c) 2010 Mohit Muthanna Cheppudira <mohit@muthanna.com>',
+    ' *',
+    ' * http://www.vexflow.com  http://github.com/0xfe/vexflow',
+    ' */',
+  ].join('\n');
   const BASE_DIR = __dirname;
   const BUILD_DIR = path.join(BASE_DIR, 'build');
   const RELEASE_DIR = path.join(BASE_DIR, 'releases');
@@ -15,7 +18,7 @@ module.exports = (grunt) => {
   const TARGET_MIN = path.join(BUILD_DIR, 'vexflow-min.js');
   const TARGET_TESTS = path.join(BUILD_DIR, 'vexflow-tests.js');
 
-  const SOURCES = ["src/*.js", "!src/header.js", "!src/container.js"];
+  const SOURCES = ['src/*.js', '!src/header.js', '!src/container.js'];
 
   const ESLINT_SOURCES = [
     'src/accidental.js',
@@ -28,24 +31,23 @@ module.exports = (grunt) => {
   const JSHINT_SOURCES = SOURCES.concat(ESLINT_SOURCES.map(negatePattern));
 
   const TEST_SOURCES = [
-    "tests/vexflow_test_helpers.js",
-    "tests/mocks.js",
-    "tests/*_tests.js",
-    "tests/run.js"
+    'tests/vexflow_test_helpers.js',
+    'tests/mocks.js',
+    'tests/*_tests.js',
+    'tests/run.js',
   ];
 
-  const babel = require('rollup-plugin-babel');
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
       options: {
         banner: BANNER,
-        sourceMap: true
+        sourceMap: true,
       },
       tests: {
         src: TEST_SOURCES,
-        dest: TARGET_TESTS
-      }
+        dest: TARGET_TESTS,
+      },
     },
     rollup: {
       options: {
@@ -57,31 +59,31 @@ module.exports = (grunt) => {
         plugins() {
           return [
             babel({
-              exclude: './node_modules/**'
-            })
+              exclude: './node_modules/**',
+            }),
           ];
-        }
+        },
       },
       files: {
         src: MODULE_ENTRY,
-        dest: TARGET_RAW
-      }
+        dest: TARGET_RAW,
+      },
     },
     uglify: {
       options: {
         banner: BANNER,
-        sourceMap: true
+        sourceMap: true,
       },
       build: {
         src: TARGET_RAW,
-        dest: TARGET_MIN
-      }
+        dest: TARGET_MIN,
+      },
     },
     eslint: {
       target: ESLINT_SOURCES,
       options: {
-        configFile: '.eslintrc.json'
-      }
+        configFile: '.eslintrc.json',
+      },
     },
     jshint: {
       files: JSHINT_SOURCES,
@@ -91,29 +93,29 @@ module.exports = (grunt) => {
         sub: true,      // don't enforce dot notation
         trailing: true, // no more trailing spaces
         globals: {
-          "Vex": false,
-          "Raphael": false
-         }
-      }
+          Vex: false,
+          Raphael: false,
+        },
+      },
     },
     qunit: {
-      files: ['tests/flow.html']
+      files: ['tests/flow.html'],
     },
     watch: {
       scripts: {
         files: ['src/*', 'Gruntfile.js'],
         tasks: ['rollup'],
         options: {
-          interrupt: true
-        }
+          interrupt: true,
+        },
       },
       tests: {
         files: ['tests/*'],
         tasks: ['concat:tests'],
         options: {
-          interrupt: true
-        }
-      }
+          interrupt: true,
+        },
+      },
     },
     copy: {
       release: {
@@ -122,31 +124,31 @@ module.exports = (grunt) => {
             expand: true,
             dest: RELEASE_DIR,
             cwd: BUILD_DIR,
-            src    : ['*.js', 'docs/**', '*.map']
-          }
-        ]
-      }
+            src: ['*.js', 'docs/**', '*.map'],
+          },
+        ],
+      },
     },
     docco: {
       src: SOURCES,
       options: {
         layout: 'linear',
-        output: 'build/docs'
-      }
+        output: 'build/docs',
+      },
     },
     gitcommit: {
       releases: {
         options: {
-          message: "Committing release binaries for new version: <%= pkg.version %>",
-          verbose: true
+          message: 'Committing release binaries for new version: <%= pkg.version %>',
+          verbose: true,
         },
         files: [
           {
             src: [`${RELEASE_DIR}/*.js`, `${RELEASE_DIR}/*.map`],
-            expand: true
-          }
-        ]
-      }
+            expand: true,
+          },
+        ],
+      },
     },
     bump: {
       options: {
@@ -154,14 +156,14 @@ module.exports = (grunt) => {
         commitFiles: ['package.json', 'component.json'],
         updateConfigs: ['pkg'],
         createTag: false,
-        push: false
-      }
+        push: false,
+      },
     },
     release: {
       options: {
         bump: false,
-        commit: false
-      }
+        commit: false,
+      },
     },
     clean: [BUILD_DIR],
   });
