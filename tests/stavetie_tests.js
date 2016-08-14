c4dccc3373e9b6471fee51c4642c8066a3046ec0
Cyril Silverman <cyril.silverman@tido-music.com>
2016-08-14 21:40:52 +0100

    Fix stavetie tests

--- tests/stavetie_tests.js
+++ tests/stavetie_tests.js
@@ -4,6 +4,9 @@
  */
 
 VF.Test.StaveTie = (function() {
+  function newNote(note_struct) { return new VF.StaveNote(note_struct); }
+  function newAcc(type) { return new VF.Accidental(type); }
+
   var StaveTie = {
     Start: function() {
       var runTests = VF.Test.runTests;
@@ -16,16 +19,12 @@ VF.Test.StaveTie = (function() {
       runTests("No Start Note", StaveTie.noStartNote);
       runTests("Set Direction Down", StaveTie.setDirectionDown);
       runTests("Set Direction Up", StaveTie.setDirectionUp);
-
     },
 
     tieNotes: function(notes, indices, stave, ctx, direction) {
-      var voice = new VF.Voice(VF.Test.TIME4_4);
-      voice.addTickables(notes);
-
-      var formatter = new VF.Formatter().joinVoices([voice]).
-        format([voice], 250);
-      voice.draw(ctx, stave);
+      var voice = new VF.Voice(VF.Test.TIME4_4)
+        .addTickables(notes)
+        .setStave(stave);
 
       var tie = new VF.StaveTie({
         first_note: notes[0],
@@ -34,19 +33,22 @@ VF.Test.StaveTie = (function() {
         last_indices: indices,
       });
 
-      tie.setContext(ctx);
+      if (!direction) tie.setDirection(direction);
 
-      if(direction !== undefined && direction !== null){
-        tie.setDirection(direction);
-      }
+      var formatter = new VF.Formatter()
+        .joinVoices([voice])
+        .format([voice], 250);
 
-      tie.draw();
+      voice.draw(ctx);
+      tie.setContext(ctx).draw();
     },
 
     drawTie: function(notes, indices, options) {
       var ctx = new options.contextBuilder(options.canvas_sel, 350, 140);
+      ctx.scale(0.9, 0.9);
+      ctx.fillStyle = "#221";
+      ctx.strokeStyle = "#221";
 
-      ctx.scale(0.9, 0.9); ctx.fillStyle = "#221"; ctx.strokeStyle = "#221";
       var stave = new VF.Stave(10, 10, 350).setContext(ctx).draw();
 
       VF.Test.StaveTie.tieNotes(notes, indices, stave, ctx, options['direction']);
@@ -58,105 +60,113 @@ VF.Test.StaveTie = (function() {
       function newAcc(type) { return new VF.Accidental(type); }
 
       VF.Test.StaveTie.drawTie([
-        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "h"}).
-          addAccidental(0, newAcc("b")).
-          addAccidental(1, newAcc("#")),
-        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"})
+        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "2" })
+          .addAccidental(0, newAcc("b"))
+          .addAccidental(1, newAcc("#")),
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "2" })
       ], [0, 1], options);
+
       ok(true, "Simple Test");
     },
 
     chord: function(options, contextBuilder) {
       options.contextBuilder = contextBuilder;
-      function newNote(note_struct) { return new VF.StaveNote(note_struct); }
-      function newAcc(type) { return new VF.Accidental(type); }
 
       VF.Test.StaveTie.drawTie([
-        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"}),
-        newNote({ keys: ["c/4", "f/4", "a/4"], stem_direction: -1, duration: "h"}).
-          addAccidental(0, newAcc("n")).
-          addAccidental(1, newAcc("#")),
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "2" }),
+        newNote({ keys: ["c/4", "f/4", "a/4"], stem_direction: -1, duration: "2" })
+          .addAccidental(0, newAcc("n"))
+          .addAccidental(1, newAcc("#")),
       ], [0, 1, 2], options);
+
       ok(true, "Chord test");
     },
 
     stemUp: function(options, contextBuilder) {
       options.contextBuilder = contextBuilder;
-      function newNote(note_struct) { return new VF.StaveNote(note_struct); }
-      function newAcc(type) { return new VF.Accidental(type); }
 
       VF.Test.StaveTie.drawTie([
-        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: 1, duration: "h"}),
-        newNote({ keys: ["c/4", "f/4", "a/4"], stem_direction: 1, duration: "h"}).
-          addAccidental(0, newAcc("n")).
-          addAccidental(1, newAcc("#")),
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: 1, duration: "2" }),
+        newNote({ keys: ["c/4", "f/4", "a/4"], stem_direction: 1, duration: "2" })
+          .addAccidental(0, newAcc("n"))
+          .addAccidental(1, newAcc("#")),
       ], [0, 1, 2], options);
+
       ok(true, "Stem up test");
     },
 
     noEndNote: function(options, contextBuilder) {
       var ctx = contextBuilder(options.canvas_sel, 350, 140);
-      ctx.scale(0.9, 0.9); ctx.fillStyle = "#221"; ctx.strokeStyle = "#221";
+      ctx.scale(0.9, 0.9);
+      ctx.fillStyle = "#221";
+      ctx.strokeStyle = "#221";
       ctx.font = "10pt Arial";
       var stave = new VF.Stave(10, 10, 350).setContext(ctx).draw();
 
-      function newNote(note_struct) { return new VF.StaveNote(note_struct); }
-      function newAcc(type) { return new VF.Accidental(type); }
-
       var notes = [
-        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "h"}).
-          addAccidental(0, newAcc("b")).
-          addAccidental(1, newAcc("#")),
-        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"})
+        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "2" })
+          .addAccidental(0, newAcc("b"))
+          .addAccidental(1, newAcc("#")),
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "2" })
       ];
 
-      var voice = new VF.Voice(VF.Test.TIME4_4);
-      voice.addTickables(notes);
-
-      var formatter = new VF.Formatter().joinVoices([voice]).
-        format([voice], 250);
-      voice.draw(ctx, stave);
+      var voice = new VF.Voice(VF.Test.TIME4_4)
+        .addTickables(notes)
+        .setStave(stave);
 
       var tie = new VF.StaveTie({
         first_note: notes[1],
         last_note: null,
         first_indices: [2],
         last_indices: [2]
-      }, "slow.").setContext(ctx).draw();
+      }, "slow.")
+
+      var formatter = new VF.Formatter()
+        .joinVoices([voice])
+        .format([voice], 250);
+
+      voice.draw(ctx);
+      tie.setContext(ctx).draw();
 
       ok(true, "No end note");
     },
 
     noStartNote: function(options, contextBuilder) {
       var ctx = contextBuilder(options.canvas_sel, 350, 140);
-      ctx.scale(0.9, 0.9); ctx.fillStyle = "#221"; ctx.strokeStyle = "#221";
+      ctx.scale(0.9, 0.9);
+      ctx.fillStyle = "#221";
+      ctx.strokeStyle = "#221";
       ctx.font = "10pt Arial";
-      var stave = new VF.Stave(10, 10, 350).addTrebleGlyph().
-        setContext(ctx).draw();
 
-      function newNote(note_struct) { return new VF.StaveNote(note_struct); }
-      function newAcc(type) { return new VF.Accidental(type); }
+      var stave = new VF.Stave(10, 10, 350)
+        .addTrebleGlyph()
+        .setContext(ctx)
+        .draw();
 
       var notes = [
-        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "h"}).
-          addAccidental(0, newAcc("b")).
-          addAccidental(1, newAcc("#")),
-        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"})
+        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "2" })
+          .addAccidental(0, newAcc("b"))
+          .addAccidental(1, newAcc("#")),
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "2" })
       ];
 
-      var voice = new VF.Voice(VF.Test.TIME4_4);
-      voice.addTickables(notes);
-
-      var formatter = new VF.Formatter().joinVoices([voice]).
-        format([voice], 250);
-      voice.draw(ctx, stave);
+      var voice = new VF.Voice(VF.Test.TIME4_4)
+        .addTickables(notes)
+        .setStave(stave);
 
       var tie = new VF.StaveTie({
         first_note: null,
         last_note: notes[0],
         first_indices: [2],
         last_indices: [2],
-      }, "H").setContext(ctx).draw();
+      }, "H");
+
+      new VF.Formatter()
+        .joinVoices([voice])
+        .format([voice], 250);
+
+      tie.setContext(ctx).draw();
+      voice.draw(ctx, stave);
 
       ok(true, "No end note");
     },
@@ -164,33 +174,30 @@ VF.Test.StaveTie = (function() {
     setDirectionDown: function(options, contextBuilder){
       options.contextBuilder = contextBuilder;
       options.direction = Vex.Flow.Stem.DOWN;
-      function newNote(note_struct) { return new VF.StaveNote(note_struct); }
-      function newAcc(type) { return new VF.Accidental(type); }
 
       VF.Test.StaveTie.drawTie([
-        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "h"}).
-            addAccidental(0, newAcc("b")).
-            addAccidental(1, newAcc("#")),
-        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"})
+        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "2" })
+          .addAccidental(0, newAcc("#"))
+          .addAccidental(1, newAcc("b")),
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "2" })
       ], [0, 1], options);
+
       ok(true, "Set Direction Down");
     },
 
     setDirectionUp: function(options, contextBuilder){
       options.contextBuilder = contextBuilder;
       options.direction = Vex.Flow.Stem.UP;
-      function newNote(note_struct) { return new VF.StaveNote(note_struct); }
-      function newAcc(type) { return new VF.Accidental(type); }
 
       VF.Test.StaveTie.drawTie([
-        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "h"}).
-            addAccidental(0, newAcc("b")).
-            addAccidental(1, newAcc("#")),
-        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "h"})
+        newNote({ keys: ["c/4", "e/4", "a/4"], stem_direction: -1, duration: "2" })
+          .addAccidental(0, newAcc("b"))
+          .addAccidental(1, newAcc("#")),
+        newNote({ keys: ["d/4", "e/4", "f/4"], stem_direction: -1, duration: "2" })
       ], [0, 1], options);
+
       ok(true, "Set Direction Down");
     }
-
   };
 
   return StaveTie;
