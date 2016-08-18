2ecc75480c0ceabb0cd98a170d4c7db094a86ed6
Mohit Cheppudira <mohit@muthanna.com>
2016-08-18 17:31:08 -0400

    Proper error management and word results.

--- src/parser.js
+++ src/parser.js
@@ -22,7 +22,19 @@ export class Parser {
   parse(line) {
     this.line = line;
     this.pos = 0;
-    return this.expect(this.grammar.begin());
+    this.errorPos = -1;
+    const results = this.expect(this.grammar.begin());
+    results.errorPos = this.errorPos;
+    return results;
+  }
+
+  matchFail(returnPos) {
+    if (this.errorPos === -1) this.errorPos = this.pos;
+    this.pos = returnPos;
+  }
+
+  matchSuccess() {
+    this.errorPos = -1;
   }
 
   matchToken(token) {
@@ -34,64 +46,104 @@ export class Parser {
         success: true,
         matchedString: result[2],
         incrementPos: result[1].length,
+        pos: this.pos,
       };
     } else {
       return {
         success: false,
+        pos: this.pos,
       };
     }
   }
 
+  expectOne(rule, maybe = false) {
+    const results = [];
+    const pos = this.pos;
+
+    let allMatches = true;
+    let oneMatch = false;
+    maybe |= (rule.maybe === true);
+
+    for (const next of rule.expect) {
+      const localPos = this.pos;
+      const result = this.expect(next);
+
+      if (result.success) {
+        results.push(result);
+        oneMatch = true;
+        if (rule.or) break;
+      } else {
+        allMatches = false;
+        if (!rule.or) {
+          this.pos = localPos;
+          break;
+        }
+      }
+    }
+
+    const gotOne = (rule.or && oneMatch) || allMatches;
+    const success = gotOne || maybe;
+    if (maybe && !gotOne) this.pos = pos;
+    if (success) this.matchSuccess(); else this.matchFail(pos);
+    return { success: (gotOne || maybe), results, gotOne };
+  }
+
+  expectOneOrMore(rule, maybe = false) {
+    const results = [];
+    const pos = this.pos;
+    let numMatches = 0;
+    let more = true;
+
+    do {
+      const result = this.expectOne(rule);
+      if (result.success) {
+        numMatches++;
+        results.push(result.results);
+      } else {
+        more = false;
+      }
+    } while (more);
+
+    const success = (numMatches > 0) || maybe;
+    if (maybe && !(numMatches > 0)) this.pos = pos;
+    if (success) this.matchSuccess(); else this.matchFail(pos);
+    return { success, results, numMatches };
+  }
+
+  expectZeroOrMore(rule) {
+    return this.expectOneOrMore(rule, true);
+  }
+
   expect(rules) {
-    const currentPos = this.pos;
+    let result;
+    const matches = [];
+    if (!rules) {
+      throw new Parser.Error('Invalid Rule: ' + rules, rules);
+    }
+
     const rule = rules.bind(this.grammar)();
     if (rule.token) {
       // Base case: parse the regex and throw an error if the
       // line doesn't match.
-      const result = this.matchToken(rule.token);
+      result = this.matchToken(rule.token);
       if (result.success) {
         // Token match! Update position and throw away parsed portion
         // of string.
         this.pos += result.incrementPos;
       }
-      return result;
     } else if (rule.expect) {
-      const results = [];
-      let numMatches = 0;
-
-      do {
-        let allMatches = true;
-        let oneMatch = false;
-        let success = false;
-
-        for (const next of rule.expect) {
-          const result = this.expect(next);
-          results.push(result);
-
-          if (result.success) {
-            oneMatch = true;
-            if (rule.or) break;
-          } else {
-            allMatches = false;
-            if (!rule.or) break;
-          }
-        }
-
-        success = (rule.or && oneMatch) || allMatches;
-        if (success) numMatches++; else break;
-      } while (rule.oneOrMore && ~rule.or);
-
-      if (numMatches > 0) {
-        if (rule.run) rule.run(results);
-        return results;
+      if (rule.oneOrMore) {
+        result = this.expectOneOrMore(rule);
+      } else if (rule.zeroOrMore) {
+        result = this.expectZeroOrMore(rule);
+      } else {
+        result = this.expectOne(rule);
       }
     } else {
       throw new Parser.Error('Bad grammar!', rule);
     }
 
-    // We failed to match this grammar, revert position.
-    const result = { success: false, pos: this.pos };
-    this.pos = currentPos;
+    if (rule.run && result.success) rule.run(result);
     return result;
   }
 }
--- tests/parser_tests.js
+++ tests/parser_tests.js
@@ -5,41 +5,101 @@
 
 Vex.Flow.Test.Parser = (function() {
   var TestGrammar = function() {
-    var results = [];
+    var words = [];
 
     return {
-      begin: function() { return this.WORDS; },
-      getResults: function() { return results; },
+      begin: function() { return this.BEGIN; },
+      resetWords: function() { words = []; },
+      getWords: function() { return words; },
+
+      BEGIN: function() { return { expect: [this.BIGLINE, this.EOL] }; },
+
+      BIGLINE: function() {
+        return {
+          expect: [this.LBRACE, this.WORD, this.WORDS, this.MAYBEEXCLAIM, this.RBRACE, this.EOL],
+          run: function(state) {words.push(state[0]);}
+        };
+      },
+
+      LITTLELINE: function() {
+        return {
+          expect: [this.WORD, this.WORDS],
+          run: function(state) { console.log("LITTLELINE: ", state); }
+        };
+      },
 
       WORDS: function() {
         return {
-          expect: [this.WORD, this.COMMA],
-          oneOrMore: true,
-          run: function(state) {results.push(state[0], state[2]);}
+          expect: [this.COMMA, this.WORD],
+          zeroOrMore: true,
+          run: function(state) { console.log("WORDS: ", state); }
          };
       },
+
+      MAYBEEXCLAIM: function() {
+        return {
+          expect: [this.EXCLAIM],
+          maybe: true,
+         };
+      },
+
+      LBRACE: function() { return { token: '[{]' }; },
+      RBRACE: function() { return { token: '[}]' }; },
       WORD:  function()  { return { token: '[a-zA-Z]+' }; },
-      COMMA: function()  { return { token: ',' }; },
+      COMMA: function()  { return { token: '[,]' }; },
+      EXCLAIM: function()  { return { token: '[!]' }; },
+      EOL: function()  { return { token: '$' }; },
     };
   };
+
+  function assertParseFail(assert, result, expectedPos, msg) {
+    assert.notOk(result.success, msg);
+    assert.equal(result.errorPos, expectedPos, msg);
+  }
 
   var Parser = {
     Start: function() {
       QUnit.module("Parser");
       var VFT = Vex.Flow.Test;
 
+      VF.Parser.DEBUG = true;
       QUnit.test("Basic", VFT.Parser.basic);
+      QUnit.test("Advanced", VFT.Parser.advanced);
     },
 
     basic: function(assert) {
-      VF.Parser.DEBUG = true;
-      const grammar = new TestGrammar();
-      const parser = new VF.Parser(grammar);
+      var grammar = new TestGrammar();
+      var parser = new VF.Parser(grammar);
 
-      console.log(parser.parse('first second'));
-      console.log(parser.parse('first, second'));
-      console.log(grammar.getResults());
+      grammar.BEGIN = function() { return { expect: [grammar.LITTLELINE, grammar.EOL] }; };
+      assertParseFail(assert, parser.parse(''), 0);
+      assertParseFail(assert, parser.parse('first second'), 6);
+      assertParseFail(assert, parser.parse('first,,'), 5);
+      assertParseFail(assert, parser.parse('first,'), 5);
+      assertParseFail(assert, parser.parse(',,'), 0);
+      assert.equal(parser.parse('first, second').success, true);
+      assert.equal(parser.parse('first,second').success, true);
+      assert.equal(parser.parse('first').success, true);
 
+      grammar.resetWords();
+      console.log(grammar.getWords());
+      assert.equal(parser.parse('first,second, third').success, true);
+      console.log(grammar.getWords());
+
+      assert.ok(true);
+    },
+
+    advanced: function(assert) {
+      var grammar = new TestGrammar();
+      var parser = new VF.Parser(grammar);
+
+      assert.equal(parser.parse('{first}').success, true);
+      assert.equal(parser.parse('{first!}').success, true);
+      assert.equal(parser.parse('{first,second}').success, true);
+      assert.equal(parser.parse('{first,second!}').success, true);
+      assert.equal(parser.parse('{first,second,third!}').success, true);
+
+      assertParseFail(assert, parser.parse('{first,second,third,}'), 19);
       assert.ok(true);
     }
   };
