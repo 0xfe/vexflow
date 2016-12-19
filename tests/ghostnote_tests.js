d30dad9104e3f3f9c418e611ce8373ea0b729314
Cyril Silverman <silverwolf90@gmail.com>
2016-12-19 12:42:44 +0000

    Refactor GhostNote tests (#491)

--- tests/ghostnote_tests.js
+++ tests/ghostnote_tests.js
@@ -1,154 +1,97 @@
 /**
  * VexFlow - Rest Tests
  * Copyright Mohit Muthanna 2010 <mohit@muthanna.com>
- *
  */
 
-VF.Test.GhostNote = (function() {
-  var GhostNote = {
-    Start: function() {
-      QUnit.module("GhostNote");
-      VF.Test.runTests("GhostNote Basic", VF.Test.GhostNote.basic);
-      VF.Test.runTests("GhostNote Dotted", VF.Test.GhostNote.dotted);
-    },
+function createTest(setup) {
+  return function(options) {
+    var vf = VF.Test.makeFactory(options, 550);
+    var stave = vf.Stave();
+    var score = vf.EasyScore();
 
-    basic: function(options, contextBuilder) {
-      var ctx = new contextBuilder(options.canvas_sel, 600, 200);
-      var stave = new VF.Stave(10, 10, 550);
-      stave.setClef('treble');
-      stave.setContext(ctx);
-      stave.draw();
+    setup(vf, score);
 
-      function newGhostNote(duration) { return new VF.GhostNote({ duration: duration })}
-      function newStaveNote(note_struct) { return new VF.StaveNote(note_struct); }
-      function addAccidental(note, type) { note.addAccidental(0, new VF.Accidental(type)); }
+    vf.Formatter()
+      .joinVoices(vf.getVoices())
+      .formatToStave(vf.getVoices(), stave);
 
-      /*
-      {} -> GhostNote
+    vf.draw();
 
-       q  q  q  q  8  8  8  8  8  8
-      {h}    q {q} q {8} 8  q
-      */
-      var notes = [
-        newStaveNote({ keys: ["f/5"], stem_direction: 1, duration: "q"}),
-        newStaveNote({ keys: ["f/5"], stem_direction: 1, duration: "q"}),
-        newStaveNote({ keys: ["d/5"], stem_direction: 1, duration: "q"}),
-        newStaveNote({ keys: ["c/5"], stem_direction: 1, duration: "q"}),
-        newStaveNote({ keys: ["c/5"], stem_direction: 1, duration: "8"}),
-        newStaveNote({ keys: ["d/5"], stem_direction: 1, duration: "8"}),
-        newStaveNote({ keys: ["f/5"], stem_direction: 1, duration: "8"}),
-        newStaveNote({ keys: ["e/5"], stem_direction: 1, duration: "8"}),
-        newStaveNote({ keys: ["d/5"], stem_direction: 1, duration: "8"}),
-        newStaveNote({ keys: ["c/5"], stem_direction: 1, duration: "8"})
-      ];
+    ok(true, 'all pass');
+  };
+}
 
-      addAccidental(notes[0], "#");
-      addAccidental(notes[2], "b");
-      addAccidental(notes[6], "n");
+VF.Test.GhostNote = {
+  Start: function() {
+    var run = VF.Test.runTests;
 
-      var notes2 = [
-        newGhostNote("h"),
-        newStaveNote({ keys: ["f/4"], stem_direction: -1, duration: "q"}),
-        newGhostNote("q"),
-        newStaveNote({ keys: ["e/4"], stem_direction: -1, duration: "q"}),
-        newGhostNote("8"),
-        newStaveNote({ keys: ["d/4"], stem_direction: -1, duration: "8"}),
-        newStaveNote({ keys: ["c/4"], stem_direction: -1, duration: "q"})
-      ];
+    QUnit.module('GhostNote');
 
-      addAccidental(notes2[5], "##");
+    run('GhostNote Basic', createTest(function(vf, score) {
+      var voice1 = score.voice(score.notes(
+        'f#5/4, f5, db5, c5, c5/8, d5, fn5, e5, d5, c5',
+        { stem: 'up' }
+      ), { time: '7/4' });
 
-      var voice = new VF.Voice(VF.Test.TIME4_4).setStrict(false);
-      var voice2 = new VF.Voice(VF.Test.TIME4_4).setStrict(false);
-      voice.addTickables(notes);
-      voice2.addTickables(notes2);
+      score.voice([
+        vf.GhostNote({ duration: '2' }),
+        vf.StaveNote({ keys: ['f/4'], stem_direction: -1, duration: '4' }),
+        vf.GhostNote({ duration: '4' }),
+        vf.StaveNote({ keys: ['e/4'], stem_direction: -1, duration: '4' }),
+        vf.GhostNote({ duration: '8' }),
+        vf.StaveNote({ keys: ['d/4'], stem_direction: -1, duration: '8' })
+          .addAccidental(0, vf.Accidental({ type: '##' })),
+        vf.StaveNote({ keys: ['c/4'], stem_direction: -1, duration: '8' }),
+        vf.StaveNote({ keys: ['c/4'], stem_direction: -1, duration: '8' }),
+      ], { time: '7/4' });
 
-      var beam = new VF.Beam(notes.slice(4, 8));
-      var beam2 = new VF.Beam(notes.slice(8, 10));
+      vf.Beam({ notes: voice1.getTickables().slice(4, 8) });
+      vf.Beam({ notes: voice1.getTickables().slice(8, 10) });
+    }));
 
-      var formatter = new VF.Formatter().joinVoices([voice, voice2]).
-        formatToStave([voice, voice2], stave);
+    run('GhostNote Dotted', createTest(function(vf, score) {
+      function addAccidental(note, type) {
+        note.addAccidental(0, vf.Accidental({ type: type }));
+      }
 
-      voice.draw(ctx, stave);
-      voice2.draw(ctx, stave);
+      var voice1 = score.voice([
+        vf.GhostNote({ duration: '4d' }),
+        vf.StaveNote({ duration: '8',  keys: ['f/5'], stem_direction: 1 }),
+        vf.StaveNote({ duration: '4',  keys: ['d/5'], stem_direction: 1 }),
+        vf.StaveNote({ duration: '8',  keys: ['c/5'], stem_direction: 1 }),
+        vf.StaveNote({ duration: '16', keys: ['c/5'], stem_direction: 1 }),
+        vf.StaveNote({ duration: '16', keys: ['d/5'], stem_direction: 1 }),
+        vf.GhostNote({ duration: '2dd' }),
+        vf.StaveNote({ duration: '8',  keys: ['f/5'], stem_direction: 1 }),
+      ], { time: '8/4' });
 
-      beam.setContext(ctx).draw();
-      beam2.setContext(ctx).draw();
+      var voice2 = score.voice([
+        vf.StaveNote({ duration: '4', keys: ['f/4'], stem_direction: -1 }),
+        vf.StaveNote({ duration: '8', keys: ['e/4'], stem_direction: -1 }),
+        vf.StaveNote({ duration: '8', keys: ['d/4'], stem_direction: -1 }),
+        vf.GhostNote({ duration: '4dd' }),
+        vf.StaveNote({ duration: '16', keys: ['c/4'], stem_direction: -1 }),
+        vf.StaveNote({ duration: '2', keys: ['c/4'], stem_direction: -1 }),
+        vf.StaveNote({ duration: '4', keys: ['d/4'], stem_direction: -1 }),
+        vf.StaveNote({ duration: '8', keys: ['f/4'], stem_direction: -1 }),
+        vf.StaveNote({ duration: '8', keys: ['e/4'], stem_direction: -1 }),
+      ], { time: '8/4' });
 
-      ok(true, "all pass");
-    },
+      var notes1 = voice1.getTickables();
+      var notes2 = voice2.getTickables();
 
-    dotted: function(options, contextBuilder) {
-      var ctx = new contextBuilder(options.canvas_sel, 600, 200);
-      var stave = new VF.Stave(10, 10, 550);
-      stave.setClef('treble');
-      stave.setContext(ctx);
-      stave.draw();
-
-      /*
-       {} -> GhostNote
-
-       {qd}    8   q    8  16  16  {hdd}       8
-        q   8  8  {qdd}        16   h    q  8  8
-      */
-      function newGhostNote(duration) { return new VF.GhostNote({ duration: duration })}
-      function newStaveNote(note_struct) { return new VF.StaveNote(note_struct); }
-      function addAccidental(note, type) { note.addAccidental(0, new VF.Accidental(type)); }
-
-      var notes = [
-        newGhostNote("qd"),
-        newStaveNote({ keys: ["f/5"], stem_direction: 1, duration: "8"}),
-        newStaveNote({ keys: ["d/5"], stem_direction: 1, duration: "q"}),
-        newStaveNote({ keys: ["c/5"], stem_direction: 1, duration: "8"}),
-        newStaveNote({ keys: ["c/5"], stem_direction: 1, duration: "16"}),
-        newStaveNote({ keys: ["d/5"], stem_direction: 1, duration: "16"}),
-        newGhostNote("hdd"),
-        newStaveNote({ keys: ["f/5"], stem_direction: 1, duration: "8"})
-      ];
-
-      addAccidental(notes[1], "bb");
-      addAccidental(notes[4], "#");
-      addAccidental(notes[7], "n");
-
-      var notes2 = [
-        newStaveNote({ keys: ["f/4"], stem_direction: -1, duration: "q"}),
-        newStaveNote({ keys: ["e/4"], stem_direction: -1, duration: "8"}),
-        newStaveNote({ keys: ["d/4"], stem_direction: -1, duration: "8"}),
-        newGhostNote("qdd"),
-        newStaveNote({ keys: ["c/4"], stem_direction: -1, duration: "16"}),
-        newStaveNote({ keys: ["c/4"], stem_direction: -1, duration: "h"}),
-        newStaveNote({ keys: ["d/4"], stem_direction: -1, duration: "q"}),
-        newStaveNote({ keys: ["f/4"], stem_direction: -1, duration: "8"}),
-        newStaveNote({ keys: ["e/4"], stem_direction: -1, duration: "8"})
-      ];
+      addAccidental(notes1[1], 'bb');
+      addAccidental(notes1[4], '#');
+      addAccidental(notes1[7], 'n');
 
       addAccidental(notes2[0], '#');
       addAccidental(notes2[4], 'b');
       addAccidental(notes2[5], '#');
       addAccidental(notes2[7], 'n');
 
-      var voice = new VF.Voice(VF.Test.TIME4_4).setStrict(false);
-      var voice2 = new VF.Voice(VF.Test.TIME4_4).setStrict(false);
-      voice.addTickables(notes);
-      voice2.addTickables(notes2);
-
-      var beam = new VF.Beam(notes.slice(3, 6));
-      var beam2 = new VF.Beam(notes2.slice(1, 3));
-      var beam3 = new VF.Beam(notes2.slice(7, 9));
-
-      var formatter = new VF.Formatter().joinVoices([voice, voice2]).
-        formatToStave([voice, voice2], stave);
-
-      voice.draw(ctx, stave);
-      voice2.draw(ctx, stave);
-
-      beam.setContext(ctx).draw();
-      beam2.setContext(ctx).draw();
-      beam3.setContext(ctx).draw();
-
-      ok(true, "all pass");
-    }
-  };
-
-  return GhostNote;
-})();
+      vf.Beam({ notes: notes1.slice(3, 6) });
+      vf.Beam({ notes: notes2.slice(1, 3) });
+      vf.Beam({ notes: notes2.slice(7, 9) });
+    }));
+  },
+};
