73017b430862542fdb6b91085102467fee5323ed
Cyril Silverman <silverwolf90@gmail.com>
2016-12-31 15:44:20 +0000

    Fix tabslide tests

--- tests/tabslide_tests.js
+++ tests/tabslide_tests.js
@@ -4,68 +4,74 @@
  */
 
 VF.Test.TabSlide = (function() {
+  function newNote(tab_struct) { return new VF.TabNote(tab_struct); }
+
+  function createMultiTest(createTabSlide) {
+    return function(options, contextBuilder) {
+      options.contextBuilder = contextBuilder;
+      VF.Test.TabSlide.multiTest(options, createTabSlide);
+    };
+  }
+
+  function setupContext(options) {
+    var ctx = options.contextBuilder(options.elementId, 350, 140);
+    ctx.scale(0.9, 0.9);
+    ctx.fillStyle = '#221';
+    ctx.strokeStyle = '#221';
+    ctx.font = '10pt Arial';
+    var stave = new VF.TabStave(10, 10, 350).addClef('tab');
+
+    return { context: ctx, stave: stave };
+  }
+
   var TabSlide = {
     Start: function() {
-      var runTests = VF.Test.runTests;
+      var run = VF.Test.runTests;
+
       QUnit.module('TabSlide');
-      runTests('Simple TabSlide', TabSlide.simple);
-      runTests('Slide Up', TabSlide.slideUp);
-      runTests('Slide Down', TabSlide.slideDown);
-    },
 
-    tieNotes: function(notes, indices, stave, ctx) {
-      var voice = new VF.Voice(VF.Test.TIME4_4)
-        .addTickables(notes)
-        .updateStave(stave);
-
-      new VF.Formatter()
-        .joinVoices([voice])
-        .format([voice], 100);
-
-      voice.draw(ctx);
-
-      var tie = new VF.TabSlide({
-        first_note: notes[0],
-        last_note: notes[1],
-        first_indices: indices,
-        last_indices: indices,
-      }, VF.TabSlide.SLIDE_UP);
-
-      tie.setContext(ctx);
-      tie.draw();
-    },
-
-    setupContext: function(options, x) {
-      var ctx = options.contextBuilder(options.elementId, 350, 140);
-      ctx.scale(0.9, 0.9);
-      ctx.fillStyle = '#221';
-      ctx.strokeStyle = '#221';
-      ctx.font = '10pt Arial';
-      var stave = new VF.TabStave(10, 10, x || 350)
-        .addTabGlyph()
-        .setContext(ctx)
-        .draw();
-
-      return { context: ctx, stave: stave };
+      run('Simple TabSlide', TabSlide.simple);
+      run('Slide Up', createMultiTest(VF.TabSlide.createSlideUp));
+      run('Slide Down', createMultiTest(VF.TabSlide.createSlideDown));
     },
 
     simple: function(options, contextBuilder) {
       options.contextBuilder = contextBuilder;
-      var c = VF.Test.TabSlide.setupContext(options);
-      function newNote(tab_struct) { return new VF.TabNote(tab_struct); }
+      var c = setupContext(options);
+      var ctx = c.context;
+      var stave = c.stave;
 
-      VF.Test.TabSlide.tieNotes([
-        newNote({ positions: [{ str: 4, fret: 4 }], duration: 'h' }),
-        newNote({ positions: [{ str: 4, fret: 6 }], duration: 'h' }),
-      ], [0], c.stave, c.context);
+      var notes = [
+        newNote({ positions: [{ str: 4, fret: 4 }], duration: '2' }),
+        newNote({ positions: [{ str: 4, fret: 6 }], duration: '2' }),
+      ];
+
+      var voice = new VF.Voice(VF.Test.TIME4_4)
+        .addTickables(notes)
+        .updateStave(stave);
+
+      var tie = new VF.TabSlide({
+        first_note: notes[0],
+        last_note: notes[1],
+        first_indices: [0],
+        last_indices: [0],
+      }, VF.TabSlide.SLIDE_UP);
+
+      new VF.Formatter()
+        .joinVoices([voice])
+        .format([voice], stave.width / 4);
+
+      stave.setContext(ctx).draw();
+      voice.draw(ctx);
+      tie.setContext(ctx).draw();
+
       ok(true, 'Simple Test');
     },
 
     multiTest: function(options, factory) {
-      var c = VF.Test.TabSlide.setupContext(options, 440, 100);
+      var c = setupContext(options);
       var ctx = c.context;
       var stave = c.stave;
-      function newNote(tab_struct) { return new VF.TabNote(tab_struct); }
 
       var notes = [
         newNote({ positions: [{ str: 4, fret: 4 }], duration: '8' }),
@@ -82,57 +88,44 @@ VF.Test.TabSlide = (function() {
         .addTickables(notes)
         .updateStave(stave);
 
+      var tabSlides = [
+        factory({
+          first_note: notes[0],
+          last_note: notes[1],
+          first_indices: [0],
+          last_indices: [0],
+        }),
+        factory({
+          first_note: notes[2],
+          last_note: notes[3],
+          first_indices: [0, 1],
+          last_indices: [0, 1],
+        }),
+        factory({
+          first_note: notes[4],
+          last_note: notes[5],
+          first_indices: [0],
+          last_indices: [0],
+        }),
+        factory({
+          first_note: notes[6],
+          last_note: notes[7],
+          first_indices: [0, 1],
+          last_indices: [0, 1],
+        }),
+      ];
+
       new VF.Formatter()
         .joinVoices([voice])
-        .format([voice], stave);
+        .formatToStave([voice], stave);
 
+      stave.setContext(ctx).draw();
       voice.draw(ctx);
+      tabSlides.forEach(function(tabSlide) {
+        tabSlide.setContext(ctx).draw();
+      });
 
-      factory({
-        first_note: notes[0],
-        last_note: notes[1],
-        first_indices: [0],
-        last_indices: [0],
-      }).setContext(ctx).draw();
-
-      ok(true, 'Single note');
-
-      factory({
-        first_note: notes[2],
-        last_note: notes[3],
-        first_indices: [0, 1],
-        last_indices: [0, 1],
-      }).setContext(ctx).draw();
-
-      ok(true, 'Chord');
-
-      factory({
-        first_note: notes[4],
-        last_note: notes[5],
-        first_indices: [0],
-        last_indices: [0],
-      }).setContext(ctx).draw();
-
-      ok(true, 'Single note high-fret');
-
-      factory({
-        first_note: notes[6],
-        last_note: notes[7],
-        first_indices: [0, 1],
-        last_indices: [0, 1],
-      }).setContext(ctx).draw();
-
-      ok(true, 'Chord high-fret');
-    },
-
-    slideUp: function(options, contextBuilder) {
-      options.contextBuilder = contextBuilder;
-      VF.Test.TabSlide.multiTest(options, VF.TabSlide.createSlideUp);
-    },
-
-    slideDown: function(options, contextBuilder) {
-      options.contextBuilder = contextBuilder;
-      VF.Test.TabSlide.multiTest(options, VF.TabSlide.createSlideDown);
+      ok(true);
     },
   };
 
