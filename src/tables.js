21a09bff3dbbc1823d706ddfd1adb3e067cb3e04
Pieter Hartzer <hartzer@hotmail.co.uk>
2018-08-27 22:46:01 +0100

    Added rectangle and square note heads and glyphs #642

--- src/tables.js
+++ src/tables.js
@@ -195,6 +195,13 @@ Flow.keyProperties.note_glyph = {
   'X1': { code: 'v95', shift_right: -0.5 },
   'X2': { code: 'v3e', shift_right: 0.5 },
   'X3': { code: 'v3b', shift_right: -2 },
+
+  /* Square */
+  'S1': { code: 'vd3', shift_right: 0 },
+  'S2': { code: 'vd2', shift_right: 0 },
+  /* Rectangle */
+  'R1': { code: 'vd5', shift_right: 0 },
+  'R2': { code: 'vd4', shift_right: 0 },
 };
 
 Flow.integerToNote = integer => {
