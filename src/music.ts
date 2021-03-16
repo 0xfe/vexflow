11956436bb0f2e51a7256b337193cbdcd28daec7
Rodrigo Vilar <rvilarl@gmail.com>
2021-03-16 18:08:44 +0100

    2nd review comments addressed

--- src/music.ts
+++ src/music.ts
@@ -4,8 +4,28 @@
 // This class implements some standard music theory routines.
 
 import { Vex } from './vex';
-import { NoteParts, NoteAccidental, Key, KeyValue, RootValue } from './types/common';
 
+export interface NoteAccidental {
+  note: number;
+  accidental: AccidentalValue;
+}
+
+export interface NoteParts {
+  root: string;
+  accidental: string;
+  type?: string;
+}
+
+export type KeyValue = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
+
+export type RootValue = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+export type AccidentalValue = -2 | -1 | 0 | 1 | 2;
+
+export interface Key {
+  root_index: RootValue;
+  int_val: KeyValue;
+}
 export class Music {
   static get NUM_TONES(): number {
     return 12;
