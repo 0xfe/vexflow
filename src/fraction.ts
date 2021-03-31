850db003c0c8a0f47eb9338a3fb7291022c341f3
Rodrigo Vilar <rvilarl@gmail.com>
2021-03-31 08:58:09 +0200

    fraction update

--- src/fraction.ts
+++ src/fraction.ts
@@ -6,14 +6,24 @@
 // @author zz85
 // @author incompleteopus (modifications)
 
-/* eslint-disable no-underscore-dangle */
-
 import { Vex } from './vex';
+
 export class Fraction {
+  numerator: number = 1;
+
+  denominator: number = 1;
+
+  // Cached objects for comparisons
+  private static __staticFractionA = new Fraction();
+
+  private static __staticFractionB = new Fraction();
+
+  private static __staticFractionTmp = new Fraction();
+
   /**
    * GCD: Find greatest common divisor using Euclidean algorithm
    */
-  static GCD(a, b) {
+  static GCD(a: number, b: number): number {
     if (typeof a !== 'number' || typeof b !== 'number') {
       throw new Vex.RERR('BadArgument', `Invalid numbers: ${a}, ${b}`);
     }
@@ -32,39 +42,47 @@ export class Fraction {
   /**
    * LCM: Lowest common multiple
    */
-  static LCM(a, b) {
+  static LCM(a: number, b: number): number {
     return (a * b) / Fraction.GCD(a, b);
   }
 
   /**
    * LCMM: Lowest common multiple for more than two numbers
    */
-  static LCMM(args) {
+  static LCMM(args: any): number {
     if (args.length === 0) {
       return 0;
-    } else if (args.length === 1) {
-      return args[0];
-    } else if (args.length === 2) {
-      return Fraction.LCM(args[0], args[1]);
-    } else {
-      const arg0 = args[0];
-      args.shift();
-      return Fraction.LCM(arg0, Fraction.LCMM(args));
     }
+    if (args.length === 1) {
+      return args[0];
+    }
+    if (args.length === 2) {
+      return Fraction.LCM(args[0], args[1]);
+    }
+    const arg0 = args[0];
+    args.shift();
+    return Fraction.LCM(arg0, Fraction.LCMM(args));
   }
 
-  constructor(numerator, denominator) {
+  /** Constructs providing numerator and denominator */
+  constructor(numerator?: number, denominator?: number) {
     this.set(numerator, denominator);
   }
-  set(numerator, denominator) {
-    this.numerator = numerator === undefined ? 1 : numerator;
-    this.denominator = denominator === undefined ? 1 : denominator;
+
+  /** Sets numerator and denominator */
+  set(numerator: number = 1, denominator: number = 1): this {
+    this.numerator = numerator;
+    this.denominator = denominator;
     return this;
   }
-  value() {
+
+  /** Returns the value of the fraction */
+  value(): number {
     return this.numerator / this.denominator;
   }
-  simplify() {
+
+  /** Simplifies numerator and denominator using GCD */
+  simplify(): this {
     let u = this.numerator;
     let d = this.denominator;
 
@@ -78,25 +96,18 @@ export class Fraction {
     }
     return this.set(u, d);
   }
-  add(param1, param2) {
-    let otherNumerator;
-    let otherDenominator;
+
+  /** Adds value of another Fraction */
+  add(param1: Fraction | number = 0, param2: number = 1): this {
+    let otherNumerator: number;
+    let otherDenominator: number;
 
     if (param1 instanceof Fraction) {
       otherNumerator = param1.numerator;
       otherDenominator = param1.denominator;
     } else {
-      if (param1 !== undefined) {
-        otherNumerator = param1;
-      } else {
-        otherNumerator = 0;
-      }
-
-      if (param2 !== undefined) {
-        otherDenominator = param2;
-      } else {
-        otherDenominator = 1;
-      }
+      otherNumerator = param1;
+      otherDenominator = param2;
     }
 
     const lcm = Fraction.LCM(this.denominator, otherDenominator);
@@ -106,25 +117,18 @@ export class Fraction {
     const u = this.numerator * a + otherNumerator * b;
     return this.set(u, lcm);
   }
-  subtract(param1, param2) {
-    let otherNumerator;
-    let otherDenominator;
+
+  /** Substract value of another Fraction */
+  subtract(param1: Fraction | number = 0, param2: number = 1): this {
+    let otherNumerator: number;
+    let otherDenominator: number;
 
     if (param1 instanceof Fraction) {
       otherNumerator = param1.numerator;
       otherDenominator = param1.denominator;
     } else {
-      if (param1 !== undefined) {
-        otherNumerator = param1;
-      } else {
-        otherNumerator = 0;
-      }
-
-      if (param2 !== undefined) {
-        otherDenominator = param2;
-      } else {
-        otherDenominator = 1;
-      }
+      otherNumerator = param1;
+      otherDenominator = param2;
     }
 
     const lcm = Fraction.LCM(this.denominator, otherDenominator);
@@ -134,133 +138,119 @@ export class Fraction {
     const u = this.numerator * a - otherNumerator * b;
     return this.set(u, lcm);
   }
-  multiply(param1, param2) {
-    let otherNumerator;
-    let otherDenominator;
+
+  /** Multiplies by value of another Fraction */
+  multiply(param1: Fraction | number = 1, param2: number = 1): this {
+    let otherNumerator: number;
+    let otherDenominator: number;
 
     if (param1 instanceof Fraction) {
       otherNumerator = param1.numerator;
       otherDenominator = param1.denominator;
     } else {
-      if (param1 !== undefined) {
-        otherNumerator = param1;
-      } else {
-        otherNumerator = 1;
-      }
-
-      if (param2 !== undefined) {
-        otherDenominator = param2;
-      } else {
-        otherDenominator = 1;
-      }
+      otherNumerator = param1;
+      otherDenominator = param2;
     }
 
     return this.set(this.numerator * otherNumerator, this.denominator * otherDenominator);
   }
-  divide(param1, param2) {
-    let otherNumerator;
-    let otherDenominator;
+
+  /** Divides by value of another Fraction */
+  divide(param1: Fraction | number = 1, param2: number = 1): this {
+    let otherNumerator: number;
+    let otherDenominator: number;
 
     if (param1 instanceof Fraction) {
       otherNumerator = param1.numerator;
       otherDenominator = param1.denominator;
     } else {
-      if (param1 !== undefined) {
-        otherNumerator = param1;
-      } else {
-        otherNumerator = 1;
-      }
-
-      if (param2 !== undefined) {
-        otherDenominator = param2;
-      } else {
-        otherDenominator = 1;
-      }
+      otherNumerator = param1;
+      otherDenominator = param2;
     }
 
     return this.set(this.numerator * otherDenominator, this.denominator * otherNumerator);
   }
 
-  // Simplifies both sides and checks if they are equal.
-  equals(compare) {
-    const a = Fraction.__compareA.copy(compare).simplify();
-    const b = Fraction.__compareB.copy(this).simplify();
+  /** Simplifies both sides and checks if they are equal. */
+  equals(compare: Fraction | number): boolean {
+    const a = Fraction.__staticFractionA.copy(compare).simplify();
+    const b = Fraction.__staticFractionB.copy(this).simplify();
 
     return a.numerator === b.numerator && a.denominator === b.denominator;
   }
 
-  // Greater than operator.
-  greaterThan(compare) {
-    const a = Fraction.__compareB.copy(this);
+  /** Greater than operator. */
+  greaterThan(compare: Fraction | number): boolean {
+    const a = Fraction.__staticFractionB.copy(this);
     a.subtract(compare);
     return a.numerator > 0;
   }
 
-  // Greater than or equals operator.
-  greaterThanEquals(compare) {
-    const a = Fraction.__compareB.copy(this);
+  /** Greater than or equals operator. */
+  greaterThanEquals(compare: Fraction | number): boolean {
+    const a = Fraction.__staticFractionB.copy(this);
     a.subtract(compare);
     return a.numerator >= 0;
   }
 
-  // Less than operator.
-  lessThan(compare) {
+  /** Less than operator. */
+  lessThan(compare: Fraction | number): boolean {
     return !this.greaterThanEquals(compare);
   }
 
-  // Less than or equals operator.
-  lessThanEquals(compare) {
+  /** Less than or equals operator. */
+  lessThanEquals(compare: Fraction | number): boolean {
     return !this.greaterThan(compare);
   }
 
-  // Creates a new copy with this current values.
-  clone() {
+  /** Creates a new copy with this current values. */
+  clone(): Fraction {
     return new Fraction(this.numerator, this.denominator);
   }
 
-  // Copies value of another Fraction into itself.
-  copy(copy) {
+  /** Copies value of another Fraction into itself. */
+  copy(copy: Fraction | number): this {
     if (typeof copy === 'number') {
       return this.set(copy || 0, 1);
     }
     return this.set(copy.numerator, copy.denominator);
   }
 
-  // Returns the integer component eg. (4/2) == 2
-  quotient() {
+  /** Returns the integer component eg. (5/2) == 2 */
+  quotient(): number {
     return Math.floor(this.numerator / this.denominator);
   }
 
-  // Returns the fraction component when reduced to a mixed number
-  fraction() {
+  /** Returns the fraction component when reduced to a mixed number */
+  fraction(): number {
     return this.numerator % this.denominator;
   }
 
-  // Returns the absolute value
-  abs() {
+  /** Calculates absolute value of itself */
+  makeAbs(): this {
     this.denominator = Math.abs(this.denominator);
     this.numerator = Math.abs(this.numerator);
     return this;
   }
 
-  // Returns a raw string representation
-  toString() {
-    return this.numerator + '/' + this.denominator;
+  /** Returns a raw string representation */
+  toString(): string {
+    return `${this.numerator}/${this.denominator}`;
   }
 
-  // Returns a simplified string respresentation
-  toSimplifiedString() {
-    return Fraction.__tmp.copy(this).simplify().toString();
+  /** Returns a simplified string respresentation */
+  toSimplifiedString(): string {
+    return Fraction.__staticFractionTmp.copy(this).simplify().toString();
   }
 
-  // Returns string representation in mixed form
-  toMixedString() {
+  /** Returns string representation in mixed form */
+  toMixedString(): string {
     let s = '';
     const q = this.quotient();
-    const f = Fraction.__tmp.copy(this);
+    const f = Fraction.__staticFractionTmp.copy(this);
 
     if (q < 0) {
-      f.abs().fraction();
+      f.makeAbs().fraction();
     } else {
       f.fraction();
     }
@@ -269,21 +259,19 @@ export class Fraction {
       s += q;
 
       if (f.numerator !== 0) {
-        s += ' ' + f.toSimplifiedString();
+        s += ` ${f.toSimplifiedString()}`;
       }
+    } else if (f.numerator === 0) {
+      s = '0';
     } else {
-      if (f.numerator === 0) {
-        s = '0';
-      } else {
-        s = f.toSimplifiedString();
-      }
+      s = f.toSimplifiedString();
     }
 
     return s;
   }
 
-  // Parses a fraction string
-  parse(str) {
+  /** Parses a fraction string */
+  parse(str: string): this {
     const i = str.split('/');
     const n = parseInt(i[0], 10);
     const d = i[1] ? parseInt(i[1], 10) : 1;
@@ -291,8 +279,3 @@ export class Fraction {
     return this.set(n, d);
   }
 }
-
-// Temporary cached objects
-Fraction.__compareA = new Fraction();
-Fraction.__compareB = new Fraction();
-Fraction.__tmp = new Fraction();
