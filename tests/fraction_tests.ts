4b212ee5f0e22ad6fc254088f60a30ede9ade6d4
Rodrigo Vilar <rvilarl@gmail.com>
2021-04-08 00:05:30 +0200

    2nd step to simplify tests

--- tests/fraction_tests.ts
+++ tests/fraction_tests.ts
@@ -6,56 +6,52 @@
 import { Fraction } from '../src/fraction';
 import { QUnit, ok, test, notOk, notEqual, strictEqual, deepEqual, notDeepEqual, notStrictEqual } from './declarations';
 
-export default (function () {
-  const FractionTests = {
-    Start: function () {
-      QUnit.module('Fraction');
-      test('Basic', FractionTests.basic);
-    },
+export const FractionTests = {
+  Start: function () {
+    QUnit.module('Fraction');
+    test('Basic', FractionTests.basic);
+  },
 
-    basic: function () {
-      const f_1_2 = new Fraction(1, 2);
-      ok(f_1_2.equals(0.5), 'Fraction: 1/2 equals 0.5');
-      ok(f_1_2.equals(new Fraction(1, 2)), 'Fraction: 1/2 equals 1/2');
-      ok(f_1_2.equals(new Fraction(2, 4)), 'Fraction: 1/2 equals 2/4');
+  basic: function () {
+    const f_1_2 = new Fraction(1, 2);
+    ok(f_1_2.equals(0.5), 'Fraction: 1/2 equals 0.5');
+    ok(f_1_2.equals(new Fraction(1, 2)), 'Fraction: 1/2 equals 1/2');
+    ok(f_1_2.equals(new Fraction(2, 4)), 'Fraction: 1/2 equals 2/4');
 
-      notOk(f_1_2.greaterThan(1), 'Fraction: ! 1/2 > 1');
-      ok(f_1_2.greaterThan(0.2), 'Fraction: 1/2 > 0.2');
+    notOk(f_1_2.greaterThan(1), 'Fraction: ! 1/2 > 1');
+    ok(f_1_2.greaterThan(0.2), 'Fraction: 1/2 > 0.2');
 
-      ok(f_1_2.greaterThanEquals(0.2), 'Fraction: 1/2 >= 0.2');
-      ok(f_1_2.greaterThanEquals(0.5), 'Fraction: 1/2 >= 0.5');
-      notOk(f_1_2.greaterThanEquals(1), 'Fraction: ! 1/2 >= 1');
+    ok(f_1_2.greaterThanEquals(0.2), 'Fraction: 1/2 >= 0.2');
+    ok(f_1_2.greaterThanEquals(0.5), 'Fraction: 1/2 >= 0.5');
+    notOk(f_1_2.greaterThanEquals(1), 'Fraction: ! 1/2 >= 1');
 
-      notOk(f_1_2.lessThan(0.5), 'Fracion: ! 1/2 < 0.5');
-      ok(f_1_2.lessThan(1), 'Fraction: 1/2 < 1');
+    notOk(f_1_2.lessThan(0.5), 'Fracion: ! 1/2 < 0.5');
+    ok(f_1_2.lessThan(1), 'Fraction: 1/2 < 1');
 
-      ok(f_1_2.lessThanEquals(0.6), 'Fraction: 1/2 <= 0.6');
-      ok(f_1_2.lessThanEquals(0.5), 'Fraction: 1/2 <= 0.5');
-      notOk(f_1_2.lessThanEquals(0.4), 'Fraction: ! 1/2 <= 0.4');
+    ok(f_1_2.lessThanEquals(0.6), 'Fraction: 1/2 <= 0.6');
+    ok(f_1_2.lessThanEquals(0.5), 'Fraction: 1/2 <= 0.5');
+    notOk(f_1_2.lessThanEquals(0.4), 'Fraction: ! 1/2 <= 0.4');
 
-      const f_05 = f_1_2.copy(0.5);
-      strictEqual(f_05, f_1_2, 'Fraction: f_05 === f_1_2');
-      strictEqual(f_05.toString(), '0.5/1', 'Fraction: f_05.toString() === "0.5/1"');
-      strictEqual(f_05.toSimplifiedString(), '1/2', 'Fraction: f_05.toSimplifiedString() === "1/2"');
+    const f_05 = f_1_2.copy(0.5);
+    strictEqual(f_05, f_1_2, 'Fraction: f_05 === f_1_2');
+    strictEqual(f_05.toString(), '0.5/1', 'Fraction: f_05.toString() === "0.5/1"');
+    strictEqual(f_05.toSimplifiedString(), '1/2', 'Fraction: f_05.toSimplifiedString() === "1/2"');
 
-      const tF_n = f_05.clone();
-      notStrictEqual(tF_n, f_05, 'Fraction: tF_n !== f_05');
-      notEqual(tF_n, f_05, 'Fraction: tF_n != f_05');
-      deepEqual(tF_n, f_05, 'tF_n deepEqual f_05');
-      notDeepEqual(tF_n, {}, 'tF_n notDeepEqual {}');
+    const tF_n = f_05.clone();
+    notStrictEqual(tF_n, f_05, 'Fraction: tF_n !== f_05');
+    notEqual(tF_n, f_05, 'Fraction: tF_n != f_05');
+    deepEqual(tF_n, f_05, 'tF_n deepEqual f_05');
+    notDeepEqual(tF_n, {}, 'tF_n notDeepEqual {}');
 
-      tF_n.subtract(-0.5);
-      ok(tF_n.equals(1), 'Fraction: 0.5 -(-0.5) equals 1');
-      tF_n.add(1);
-      ok(tF_n.equals(2), 'Fraction: 1 + 1 equals 2');
-      tF_n.multiply(2);
-      ok(tF_n.equals(4), 'Fraction: 2 * 2 equals 4');
-      tF_n.divide(2);
-      ok(tF_n.equals(2), 'Fraction: 4 / 2 equals 2');
+    tF_n.subtract(-0.5);
+    ok(tF_n.equals(1), 'Fraction: 0.5 -(-0.5) equals 1');
+    tF_n.add(1);
+    ok(tF_n.equals(2), 'Fraction: 1 + 1 equals 2');
+    tF_n.multiply(2);
+    ok(tF_n.equals(4), 'Fraction: 2 * 2 equals 4');
+    tF_n.divide(2);
+    ok(tF_n.equals(2), 'Fraction: 4 / 2 equals 2');
 
-      // TODO: Add more detailed tests.
-    },
-  };
-
-  return FractionTests;
-})();
+    // TODO: Add more detailed tests.
+  },
+};
--- tests/tuning_tests.ts
+++ tests/tuning_tests.ts
@@ -6,98 +6,94 @@
 import { Tuning } from '../src/tuning';
 import { QUnit, test, equal, expect } from './declarations';
 
-export default (function () {
-  const TuningTests = {
-    Start: function () {
-      QUnit.module('Tuning');
-      test('Standard Tuning', TuningTests.standard);
-      test('Standard Banjo Tuning', TuningTests.banjo);
-      test('Return note for fret', TuningTests.noteForFret);
-    },
+export const TuningTests = {
+  Start: function () {
+    QUnit.module('Tuning');
+    test('Standard Tuning', TuningTests.standard);
+    test('Standard Banjo Tuning', TuningTests.banjo);
+    test('Return note for fret', TuningTests.noteForFret);
+  },
 
-    checkStandard: function (tuning: Tuning) {
-      try {
-        tuning.getValueForString(0);
-      } catch (e) {
-        equal(e.code, 'BadArguments', 'String 0');
-      }
+  checkStandard: function (tuning: Tuning) {
+    try {
+      tuning.getValueForString(0);
+    } catch (e) {
+      equal(e.code, 'BadArguments', 'String 0');
+    }
 
-      try {
-        tuning.getValueForString(9);
-      } catch (e) {
-        equal(e.code, 'BadArguments', 'String 7');
-      }
+    try {
+      tuning.getValueForString(9);
+    } catch (e) {
+      equal(e.code, 'BadArguments', 'String 7');
+    }
 
-      equal(tuning.getValueForString(6), 40, 'Low E string');
-      equal(tuning.getValueForString(5), 45, 'A string');
-      equal(tuning.getValueForString(4), 50, 'D string');
-      equal(tuning.getValueForString(3), 55, 'G string');
-      equal(tuning.getValueForString(2), 59, 'B string');
-      equal(tuning.getValueForString(1), 64, 'High E string');
-    },
+    equal(tuning.getValueForString(6), 40, 'Low E string');
+    equal(tuning.getValueForString(5), 45, 'A string');
+    equal(tuning.getValueForString(4), 50, 'D string');
+    equal(tuning.getValueForString(3), 55, 'G string');
+    equal(tuning.getValueForString(2), 59, 'B string');
+    equal(tuning.getValueForString(1), 64, 'High E string');
+  },
 
-    checkStandardBanjo: function (tuning: Tuning) {
-      try {
-        tuning.getValueForString(0);
-      } catch (e) {
-        equal(e.code, 'BadArguments', 'String 0');
-      }
+  checkStandardBanjo: function (tuning: Tuning) {
+    try {
+      tuning.getValueForString(0);
+    } catch (e) {
+      equal(e.code, 'BadArguments', 'String 0');
+    }
 
-      try {
-        tuning.getValueForString(6);
-      } catch (e) {
-        equal(e.code, 'BadArguments', 'String 6');
-      }
+    try {
+      tuning.getValueForString(6);
+    } catch (e) {
+      equal(e.code, 'BadArguments', 'String 6');
+    }
 
-      equal(tuning.getValueForString(5), 67, 'High G string');
-      equal(tuning.getValueForString(4), 50, 'D string');
-      equal(tuning.getValueForString(3), 55, 'G string');
-      equal(tuning.getValueForString(2), 59, 'B string');
-      equal(tuning.getValueForString(1), 62, 'High D string');
-    },
+    equal(tuning.getValueForString(5), 67, 'High G string');
+    equal(tuning.getValueForString(4), 50, 'D string');
+    equal(tuning.getValueForString(3), 55, 'G string');
+    equal(tuning.getValueForString(2), 59, 'B string');
+    equal(tuning.getValueForString(1), 62, 'High D string');
+  },
 
-    banjo: function () {
-      expect(7);
+  banjo: function () {
+    expect(7);
 
-      const tuning = new Tuning();
-      tuning.setTuning('standardBanjo');
-      TuningTests.checkStandardBanjo(tuning);
-    },
+    const tuning = new Tuning();
+    tuning.setTuning('standardBanjo');
+    TuningTests.checkStandardBanjo(tuning);
+  },
 
-    standard: function () {
-      expect(16);
+  standard: function () {
+    expect(16);
 
-      const tuning = new Tuning();
-      TuningTests.checkStandard(tuning);
+    const tuning = new Tuning();
+    TuningTests.checkStandard(tuning);
 
-      // Test named tuning
-      tuning.setTuning('standard');
-      TuningTests.checkStandard(tuning);
-    },
+    // Test named tuning
+    tuning.setTuning('standard');
+    TuningTests.checkStandard(tuning);
+  },
 
-    noteForFret: function () {
-      expect(8);
-      const tuning = new Tuning('E/5,B/4,G/4,D/4,A/3,E/3');
-      try {
-        tuning.getNoteForFret(-1, 1);
-      } catch (e) {
-        equal(e.code, 'BadArguments', 'Fret -1');
-      }
+  noteForFret: function () {
+    expect(8);
+    const tuning = new Tuning('E/5,B/4,G/4,D/4,A/3,E/3');
+    try {
+      tuning.getNoteForFret(-1, 1);
+    } catch (e) {
+      equal(e.code, 'BadArguments', 'Fret -1');
+    }
 
-      try {
-        tuning.getNoteForFret(1, -1);
-      } catch (e) {
-        equal(e.code, 'BadArguments', 'String -1');
-      }
+    try {
+      tuning.getNoteForFret(1, -1);
+    } catch (e) {
+      equal(e.code, 'BadArguments', 'String -1');
+    }
 
-      equal(tuning.getNoteForFret(0, 1), 'E/5', 'High E string');
-      equal(tuning.getNoteForFret(5, 1), 'A/5', 'High E string, fret 5');
-      equal(tuning.getNoteForFret(0, 2), 'B/4', 'B string');
-      equal(tuning.getNoteForFret(0, 3), 'G/4', 'G string');
-      equal(tuning.getNoteForFret(12, 2), 'B/5', 'B string, fret 12');
-      equal(tuning.getNoteForFret(0, 6), 'E/3', 'Low E string');
-    },
-  };
-
-  return TuningTests;
-})();
+    equal(tuning.getNoteForFret(0, 1), 'E/5', 'High E string');
+    equal(tuning.getNoteForFret(5, 1), 'A/5', 'High E string, fret 5');
+    equal(tuning.getNoteForFret(0, 2), 'B/4', 'B string');
+    equal(tuning.getNoteForFret(0, 3), 'G/4', 'G string');
+    equal(tuning.getNoteForFret(12, 2), 'B/5', 'B string, fret 12');
+    equal(tuning.getNoteForFret(0, 6), 'E/3', 'Low E string');
+  },
+};
