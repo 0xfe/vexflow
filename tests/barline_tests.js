8efec9e2b0f5e113d7d02175193c3ca0d61d2f4a
Rodrigo Vilar <rvilarl@gmail.com>
2021-05-07 05:59:40 +0200

    PR #609 test added

--- tests/barline_tests.js
+++ tests/barline_tests.js
@@ -31,6 +31,28 @@ VF.Test.Barline = (function () {
 
         ok(true, 'Simple Test');
       });
+      run('Style BarNotes', function (options) {
+        var vf = VF.Test.makeFactory(options, 380, 160);
+        var stave = vf.Stave();
+
+        var notes = [
+          vf.StaveNote({ keys: ['d/4', 'e/4', 'f/4'], stem_direction: -1, duration: '2' }),
+          vf.BarNote({ type: 'single' }),
+          vf
+            .StaveNote({ keys: ['c/4', 'f/4', 'a/4'], stem_direction: -1, duration: '2' })
+            .addAccidental(0, vf.Accidental({ type: 'n' }))
+            .addAccidental(1, vf.Accidental({ type: '#' })),
+        ];
+        notes[1].setStyle({ shadowBlur: 15, shadowColor: 'blue', fillStyle: 'blue', strokeStyle: 'blue' });
+
+        var voice = vf.Voice().addTickables(notes);
+
+        vf.Formatter().joinVoices([voice]).formatToStave([voice], stave);
+
+        vf.draw();
+
+        ok(true, 'Style');
+      });
     },
   };
 })();
