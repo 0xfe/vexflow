78d618e39903c8207045c52b8f3c26e03fcfed48
Rodrigo Vilar <rvilarl@gmail.com>
2021-05-08 17:31:40 +0200

    review comments

--- src/crescendo.ts
+++ src/crescendo.ts
@@ -125,8 +125,11 @@ export class Crescendo extends Note {
     L('Drawing ', this.decrescendo ? 'decrescendo ' : 'crescendo ', this.height, 'x', begin_x - end_x);
 
     renderHairpin(ctx, {
+      // eslint-disable-next-line
       begin_x: begin_x - this.render_options.extend_left!,
+      // eslint-disable-next-line
       end_x: end_x + this.render_options.extend_right!,
+      // eslint-disable-next-line
       y: y + this.render_options.y_shift!,
       height: this.height,
       reverse: this.decrescendo,
