166dfd44961da814017f5d1cffd76efb753e0bdd
Ron B. Yeh <ronyeh@gmail.com>
2021-12-09 00:42:47 -0800

    Add a README. Cleanup.

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
 export const VERSION: string = '4.0.0';
-export const BUILD: string = '93c98abf7653c1667eab566bdee74e98410f5bb2';
+export const BUILD: string = '9b46e51d13c77870fe742e3eae9a1777df489d95';
