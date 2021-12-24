3ac39b2607dec8b4fb1005bae6b49a9be3fadac8
Ron B. Yeh <ronyeh@gmail.com>
2021-12-23 23:54:41 -1000

    Fix package.json types field.

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const VERSION: string = '4.0.0';
-export const ID: string = '437d802e9280e626a74adb2ecaec6cbe669b1b2d';
-export const DATE: string = '2021-12-24T07:48:55.675Z';
+export const ID: string = '109389bccab8a1bec421a9a4118515a3bc0e0de2';
+export const DATE: string = '2021-12-24T09:52:12.732Z';
