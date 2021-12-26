d8befb3a596d91c2c015d002cab07a4ed9fcb7fd
Rodrigo Vilar <rvilarl@gmail.com>
2021-12-26 08:57:47 +0100

    use actualBoundingBox when fontBoundingBox is not available

--- src/canvascontext.ts
+++ src/canvascontext.ts
@@ -258,12 +258,22 @@ export class CanvasContext extends RenderContext {
 
   measureText(text: string): TextMeasure {
     const metrics = this.context2D.measureText(text);
+
+    let y = 0;
+    let height = 0;
+    if (metrics.fontBoundingBoxAscent) {
+      y = -metrics.fontBoundingBoxAscent;
+      height = metrics.fontBoundingBoxDescent + metrics.fontBoundingBoxAscent;
+    } else {
+      y = -metrics.actualBoundingBoxAscent;
+      height = metrics.actualBoundingBoxDescent + metrics.actualBoundingBoxAscent;
+    }
     // Return x, y, width & height in the same manner as svg getBBox
     return {
       x: 0,
-      y: -metrics.fontBoundingBoxAscent,
+      y: y,
       width: metrics.width,
-      height: metrics.fontBoundingBoxDescent + metrics.fontBoundingBoxAscent,
+      height: height,
     };
   }
 
