4adc3bf56cc022b6ee1d96d363abecb58c535100
Ron B. Yeh <ronyeh@gmail.com>
2022-01-10 00:32:34 -0800

    package.json files list.

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const VERSION: string = '4.0.0';
-export const ID: string = 'c621f2f3f43e8c20dd5f4a95c6891580a08b912b';
-export const DATE: string = '2022-01-10T08:27:36.722Z';
+export const ID: string = 'fafe6706632ec0ee2f6859b4f91ebcefb0cf5a5b';
+export const DATE: string = '2022-01-10T08:29:35.590Z';
