59d80942c09e7cd14fc395ec5a1b8b882ab3a5c3
Ron B. Yeh <ronyeh@gmail.com>
2022-01-11 20:07:14 -0800

    ESM watch.

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const VERSION: string = '4.0.0';
-export const ID: string = 'fe24e18d1b7c1b308d04f568501eaf76d156067b';
-export const DATE: string = '2022-01-12T00:52:41.417Z';
+export const ID: string = '296837fecd8f1c1e4982a7d0e8919d26d54647d0';
+export const DATE: string = '2022-01-12T04:04:37.416Z';
