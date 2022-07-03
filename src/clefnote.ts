34bc4c86707cc320f04161b76729046ef23404df
Rodrigo Vilar <rvilarl@gmail.com>
2022-07-03 15:10:04 +0200

    clefnote inherits from glyphnote

--- src/clefnote.ts
+++ src/clefnote.ts
@@ -2,34 +2,26 @@
 // Author Taehoon Moon 2014
 // MIT License
 
-import { BoundingBox } from './boundingbox';
 import { Clef, ClefType } from './clef';
 import { Glyph } from './glyph';
-import { ModifierContext } from './modifiercontext';
-import { Note } from './note';
-import { RenderContext } from './rendercontext';
+import { GlyphNote } from './glyphnote';
 import { Category } from './typeguard';
 
 /** ClefNote implements clef annotations in measures. */
-export class ClefNote extends Note {
+export class ClefNote extends GlyphNote {
   static get CATEGORY(): string {
     return Category.ClefNote;
   }
 
-  protected clef_obj: Clef;
+  protected clef: Clef;
   protected type: string;
-  protected clef: ClefType;
-  protected glyph: Glyph;
 
   constructor(type: string, size?: string, annotation?: string) {
-    super({ duration: 'b' });
-
+    const clef = new Clef(type, size, annotation);
+    const glyph = new Glyph(clef.clef.code, clef.clef.point);
+    super(glyph, { duration: 'b' });
     this.type = type;
-    this.clef_obj = new Clef(type, size, annotation);
-    this.clef = this.clef_obj.clef;
-    this.glyph = new Glyph(this.clef.code, this.clef.point);
-    this.setWidth(this.glyph.getMetrics().width);
-
+    this.clef = clef;
     // Note properties
     this.ignore_ticks = true;
   }
@@ -37,40 +29,13 @@ export class ClefNote extends Note {
   /** Set clef type, size and annotation. */
   setType(type: string, size: string, annotation: string): this {
     this.type = type;
-    this.clef_obj = new Clef(type, size, annotation);
-    this.clef = this.clef_obj.clef;
-    this.glyph = new Glyph(this.clef.code, this.clef.point);
-    this.setWidth(this.glyph.getMetrics().width);
-    return this;
+    this.clef = new Clef(type, size, annotation);
+    return this.setGlyph(new Glyph(this.clef.clef.code, this.clef.clef.point));
   }
 
   /** Get associated clef. */
   getClef(): ClefType {
-    return this.clef;
-  }
-
-  /** Set associated context. */
-  setContext(context: RenderContext): this {
-    super.setContext(context);
-    this.glyph.setContext(this.getContext());
-    return this;
-  }
-
-  /** Get bounding box. */
-  getBoundingBox(): BoundingBox | undefined {
-    return super.getBoundingBox();
-  }
-
-  /* Overridden to ignore */
-  // eslint-disable-next-line
-  addToModifierContext(mc: ModifierContext): this {
-    // DO NOTHING.
-    return this;
-  }
-
-  preFormat(): this {
-    this.preFormatted = true;
-    return this;
+    return this.clef.clef;
   }
 
   /** Render clef note. */
@@ -84,18 +49,18 @@ export class ClefNote extends Note {
     const abs_x = this.getAbsoluteX();
 
     this.glyph.setStave(stave);
-    this.glyph.setYShift(stave.getYForLine(this.clef.line ?? 0) - stave.getYForGlyphs());
+    this.glyph.setYShift(stave.getYForLine(this.clef.clef.line ?? 0) - stave.getYForGlyphs());
     this.glyph.renderToStave(abs_x);
 
     // If the Vex.Flow.Clef has an annotation, such as 8va, draw it.
-    if (this.clef_obj.annotation !== undefined) {
-      const attachment = new Glyph(this.clef_obj.annotation.code, this.clef_obj.annotation.point);
+    if (this.clef.annotation !== undefined) {
+      const attachment = new Glyph(this.clef.annotation.code, this.clef.annotation.point);
       if (!attachment.getContext()) {
         attachment.setContext(this.getContext());
       }
       attachment.setStave(stave);
-      attachment.setYShift(stave.getYForLine(this.clef_obj.annotation.line) - stave.getYForGlyphs());
-      attachment.setXShift(this.clef_obj.annotation.x_shift);
+      attachment.setYShift(stave.getYForLine(this.clef.annotation.line) - stave.getYForGlyphs());
+      attachment.setXShift(this.clef.annotation.x_shift);
       attachment.renderToStave(abs_x);
     }
   }
