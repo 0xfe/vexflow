3bf7a87f34dff7662d428a6ca5c2fa6d5035e5a1
Ron B. Yeh <ronyeh@gmail.com>
2022-10-09 21:49:47 -0700

    Remove build/ after releasing version 4.1.0-alpha.0 to npm and GitHub.

--- build/esm/entry/vexflow-debug.js
+++
@@ -1,8 +0,0 @@
-import { Flow } from '../src/flow.js';
-import { loadAllMusicFonts } from '../src/fonts/load_all.js';
-import { loadTextFonts } from '../src/fonts/textfonts.js';
-loadAllMusicFonts();
-Flow.setMusicFont('Bravura', 'Gonville', 'Custom');
-loadTextFonts();
-export * from '../src/index.js';
-export * as default from '../src/index.js';
--- build/types/src/repeatnote.d.ts
+++
@@ -1,6 +0,0 @@
-import { GlyphNote, GlyphNoteOptions } from './glyphnote';
-import { NoteStruct } from './note';
-export declare class RepeatNote extends GlyphNote {
-    static get CATEGORY(): string;
-    constructor(type: string, noteStruct?: NoteStruct, options?: GlyphNoteOptions);
-}
