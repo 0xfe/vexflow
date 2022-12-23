03c7d6f6cd5c60c392176d29eb8454d7b2b5ccaa
Ron B. Yeh <ronyeh@gmail.com>
2022-12-23 12:27:27 -0800

    Remove build/ after releasing version 4.2.0-beta.0 to npm and GitHub.

--- build/esm/src/fonts/load_gonville.js
+++
@@ -1,6 +0,0 @@
-import { Font } from '../font.js';
-import { GonvilleSmuflFont } from './gonville_glyphs.js';
-import { GonvilleMetrics } from './gonville_metrics.js';
-export function loadGonville() {
-    Font.load('Gonville', GonvilleSmuflFont, GonvilleMetrics);
-}
--- build/esm/src/version.js
+++
@@ -1,3 +0,0 @@
-export const VERSION = '4.2.0-beta.0';
-export const ID = '287a4f978cf04cd00061ef1de5be3edadb45b71c';
-export const DATE = '2022-12-23T18:55:17.223Z';
