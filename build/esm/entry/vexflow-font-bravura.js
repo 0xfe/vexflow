ee1e01d90db1ed5a6ecf3c46728c93d26285b26d
Ron B. Yeh <ronyeh@gmail.com>
2023-06-05 18:15:07 -0700

    Remove build/ after releasing version 4.2.0-rc.0 to npm and GitHub.

--- build/esm/entry/vexflow-font-bravura.js
+++
@@ -1,7 +0,0 @@
-import { BravuraFont } from '../src/fonts/bravura_glyphs.js';
-import { CommonMetrics } from '../src/fonts/common_metrics.js';
-export const Font = {
-    data: BravuraFont,
-    metrics: CommonMetrics,
-};
-export default Font;
--- build/esm/src/version.js
+++
@@ -1,3 +0,0 @@
-export const VERSION = '4.2.0-rc.0';
-export const ID = 'fa66018377f6514021e705f5cd47d4050e93e104';
-export const DATE = '2023-06-05T21:03:29.286Z';
