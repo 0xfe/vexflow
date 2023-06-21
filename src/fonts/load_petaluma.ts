625ab611f476beb0cbcf0a644696a71c312d4c8b
Ron B. Yeh <ronyeh@gmail.com>
2023-06-21 14:19:03 -0700

    eslint autofix.

--- src/fonts/load_petaluma.ts
+++ src/fonts/load_petaluma.ts
@@ -1,11 +1,10 @@
 // [VexFlow](https://vexflow.com) - Copyright (c) Mohit Muthanna 2010.
 // MIT License
 
-import { Font } from '../font';
+import { ChordSymbolGlyphMetrics } from '../chordsymbol';
+import { Font, FontMetrics } from '../font';
 import { CommonMetrics } from './common_metrics';
 import { PetalumaFont } from './petaluma_glyphs';
-import { ChordSymbolGlyphMetrics } from '../chordsymbol';
-import { FontMetrics } from '../font';
 
 const petalumaChordMetrics: Record<string, ChordSymbolGlyphMetrics> = {
   csymDiminished: {
@@ -77,8 +76,9 @@ const petalumaChordMetrics: Record<string, ChordSymbolGlyphMetrics> = {
     leftSideBearing: -10,
     advanceWidth: 228,
     yOffset: -284,
-  }
-}
+  },
+};
+
 export function loadPetaluma() {
   const metrics: FontMetrics = JSON.parse(JSON.stringify(CommonMetrics));
   const chordMetrics = metrics.chordSymbol;
