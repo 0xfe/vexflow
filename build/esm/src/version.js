19ddc353ae3c3b71c1c5a6ca96a58913e8bc6faa
Ron B. Yeh <ronyeh@gmail.com>
2023-06-21 13:25:45 -0700

    Remove build/ after releasing version 4.2.1 to npm and GitHub.

--- build/esm/src/version.js
+++
@@ -1,3 +0,0 @@
-export const VERSION = '4.2.1';
-export const ID = '6c05deea44902a09a017507a8ddc810d2e2a9922';
-export const DATE = '2023-06-21T20:24:17.916Z';
--- build/types/src/gracetabnote.d.ts
+++
@@ -1,5 +0,0 @@
-import { TabNote, TabNoteStruct } from './tabnote';
-export declare class GraceTabNote extends TabNote {
-    static get CATEGORY(): string;
-    constructor(noteStruct: TabNoteStruct);
-}
