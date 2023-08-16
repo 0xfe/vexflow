207dc59e3a6659854898c488a0a186dbcc189acd
Ron B. Yeh <ronyeh@gmail.com>
2023-08-16 00:08:17 -0700

    Remove build/ after releasing version 4.2.3 to npm and GitHub.

--- build/esm/src/fonts/load_gonville.js
+++
@@ -1,6 +0,0 @@
-import { Font } from '../font.js';
-import { CommonMetrics } from './common_metrics.js';
-import { GonvilleSmuflFont } from './gonville_glyphs.js';
-export function loadGonville() {
-    Font.load('Gonville', GonvilleSmuflFont, CommonMetrics);
-}
