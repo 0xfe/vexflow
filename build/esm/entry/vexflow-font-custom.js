965a2d983ce27d972e44643de4884c6c8cd199ed
Ron B. Yeh <ronyeh@gmail.com>
2024-05-10 13:41:52 -0700

    Remove build/ after releasing version 4.2.4 to npm and GitHub.

--- build/esm/entry/vexflow-font-custom.js
+++
@@ -1,7 +0,0 @@
-import { CommonMetrics } from '../src/fonts/common_metrics.js';
-import { CustomFont } from '../src/fonts/custom_glyphs.js';
-export const Font = {
-    data: CustomFont,
-    metrics: CommonMetrics,
-};
-export default Font;
--- build/esm/src/version.js
+++
@@ -1,3 +0,0 @@
-export const VERSION = '4.2.4';
-export const ID = 'c1d8ac265fb8d29a2930e4e36296a7635c2fe12c';
-export const DATE = '2024-05-10T20:40:52.394Z';
