783af8671d6d5c6861ba80505b4cfa7834e30131
Ron B. Yeh <ronyeh@gmail.com>
2024-05-12 01:57:35 -0700

    Remove build/ after releasing version 4.2.5 to npm and GitHub.

--- build/esm/entry/vexflow-font-leland.js
+++
@@ -1,7 +0,0 @@
-import { CommonMetrics } from '../src/fonts/common_metrics.js';
-import { LelandFont } from '../src/fonts/leland_glyphs.js';
-export const Font = {
-    data: LelandFont,
-    metrics: CommonMetrics,
-};
-export default Font;
--- build/esm/src/fonts/load_custom.js
+++
@@ -1,6 +0,0 @@
-import { Font } from '../font.js';
-import { CommonMetrics } from './common_metrics.js';
-import { CustomFont } from './custom_glyphs.js';
-export function loadCustom() {
-    Font.load('Custom', CustomFont, CommonMetrics);
-}
